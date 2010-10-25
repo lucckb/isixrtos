5741c2cdbd28c0f14d6e4d8e5deb6434d84315bc
Lucjan Bryndza <lbryndza.p@boff.pl>
2010-10-25 20:55:32 +0200

    Redefine macro for proper usage

--- include/dbglog.h
+++ include/dbglog.h
@@ -12,30 +12,31 @@
 #ifdef PDEBUG /* Debug enabled */
 #include <tiny_printf.h>
 
-
+#pragma GCC system_header
 
 #ifdef __cplusplus
 
-#define dblog_init(function,arg) fnd::register_printf_putc_handler(function,arg)
-#define dbprintf(fmt,...) fnd::tiny_printf("%s:%d|"fmt"\r\n",__FILE__,__LINE__,__VA_ARGS__)
-#define dbputs(str) fnd::tiny_printf("%s:%d|"str"\r\n",__FILE__,__LINE__)
+#define dblog_init(function,arg, usart_init,...)	\
+  do { fnd::register_printf_putc_handler(function,arg); \
+  	   usart_init(__VA_ARGS__); } while(0)
+#define dbprintf(fmt, ...) fnd::tiny_printf("%s:%d|"fmt"\r\n",__FILE__,__LINE__,## __VA_ARGS__)
 
 #else
 
-#define dblog_init(function,arg) register_printf_putc_handler(function,arg)
-#define dbprintf(fmt,...) tiny_printf("%s:%d|"fmt"\r\n",__FILE__,__LINE__,__VA_ARGS__)
-#define dbputs(str) tiny_printf("%s:%d|"str"\r\n",__FILE__,__LINE__)
+#define dblog_init(function,arg, usart_init,...)	\
+  do { register_printf_putc_handler(function,arg); \
+  	   usart_init(__VA_ARGS__); } while(0)
+#define dbprintf(fmt, ...) tiny_printf("%s:%d|"fmt"\r\n",__FILE__,__LINE__,## __VA_ARGS__)
+
 
 #endif /*__cplusplus */
 
 
 #else
 
-#define dblog_init(function,arg)
+#define dblog_init(function,arg, usart_init,...) do {} while(0)
+#define dbprintf(fmt,...) do {} while(0)
 
-#define dbprintf(fmt,...)
-
-#define dbputs(str)
 
 #endif /* PDEBUG */
 
