ea2c655f601663d158c03c34078235f5f3e794ca
Lucjan Bryndza <lbryndza.p@boff.pl>
2011-03-11 20:06:34 +0100

    To lib convert updates

--- isixdrv.mk
+++ isixdrv.mk
@@ -4,4 +4,14 @@ ISIXDRV_CPPSRC += $(ISIXDRV_DIR)/usart_buffered.cpp
 
 ISIXDRV_INC += -I$(ISIXDRV_DIR)/include
 
+ISIXDRV_LIB = $(ISIXDRV_DIR)/libisixdrv-stm32.a
+ISIXDRV_OBJS += $(ISIXDRV_SRC:%.c=%.o) $(ISIXDRV_CPPSRC:%.cpp=%.o)
+DEPFILES += $(ISIXDRV_SRC:%.c=%.dep) $(ISIXDRV_CPPSRC:%.cpp=%.dep)
+
+$(ISIXDRV_LIB): $(ISIXDRV_OBJS)
+	    $(AR) $(ARFLAGS) $@ $^
+
+LIBS += $(ISIXDRV_LIB)
+LIBS_OBJS += $(ISIXDRV_OBJS)
+
 
