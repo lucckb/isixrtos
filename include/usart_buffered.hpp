bf0cc8340538523d328d2e92e967efaac13e0c4c
karp <maciej@karpiuk.pl>
2011-03-24 08:48:45 +0100

    Added rx avail

--- include/usart_buffered.hpp
+++ include/usart_buffered.hpp
@@ -84,6 +84,7 @@ public:
 	//Get string into the uart
 	int gets(char *str, std::size_t max_len, isix::tick_t timeout=isix::ISIX_TIME_INFINITE);
 	int get(void *buf, std::size_t max_len, isix::tick_t timeout);
+	int rx_avail() const { return rx_queue.size(); }
 protected:
 	virtual void before_tx() {}
 	virtual void after_tx() {}
