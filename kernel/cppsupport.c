9d3dc210be5f507ddab44b8ad7b553bd068d2fa2
Lucjan Bryndza <lbryndza.p@boff.pl>
2011-04-14 18:22:57 +0200

    Move malloc_r to libfoundation it should be at foundation

--- kernel/cppsupport.c
+++ kernel/cppsupport.c
@@ -59,14 +59,5 @@ int * __errno(void)
 	return &isix_current_task->thread_errno;
 }
 
-void *_malloc_r(struct _reent *r, size_t size)
-{
-	return isix_alloc(size);
-}
-
-void _free_r(struct _reent *r, void *ptr)
-{
-	isix_free(ptr);
-}
 #endif
 
