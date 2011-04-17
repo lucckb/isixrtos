cd297da4d965eaccbfffbca7cf495aae3caf537c
Lucjan Bryndza <lbryndza.p@boff.pl>
2011-04-17 11:15:46 +0200

    Removed errno, errno is global back because full errno support will require special version of compiler with per thread reent struct

--- include/prv/scheduler.h
+++ include/prv/scheduler.h
@@ -41,9 +41,6 @@ struct task_struct
     list_t inode_sem;           	//Inode of semaphore
     sem_t   *sem;               	//Pointer to waiting sem
     list_t inode;               	//List of tasks
-#ifdef ISIX_CONFIG_NEWLIB_SUPPORT
-    int thread_errno;			//Errno per thread
-#endif
 };
 /*-----------------------------------------------------------------------*/
 
