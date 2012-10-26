cd7762a43b1280c8208a304c1d509a840d45f44f
Lucjan Bryndza <lbryndza.p@boff.pl>
2012-10-26 21:14:25 +0200

    Added inital version of LWIP

---
+++ libtcpip/libtcpip.mk
@@ -0,0 +1,64 @@
+#Lwip library version
+LWIP_VERSION := 1.4.0
+
+#LWIP base dir
+LWIPDIR := $(LIBTCPIP_DIR)/lwip-$(LWIP_VERSION)/src
+
+#Architecture dependend dir
+LWIPPORTDIR := $(LIBTCPIP_DIR)/port
+
+
+#CFLAGS for hardware checksum
+CFLAGS 	 += -DISIX_TCPIPLIB_CHECKSUM_BY_HARDWARE
+CXXFLAGS += -DISIX_TCPIPLIB_CHECKSUM_BY_HARDWARE
+
+
+# COREFILES, CORE4FILES: The minimum set of files needed for lwIP.
+COREFILES=$(LWIPDIR)/core/mem.c $(LWIPDIR)/core/memp.c $(LWIPDIR)/core/netif.c \
+	$(LWIPDIR)/core/pbuf.c $(LWIPDIR)/core/raw.c $(LWIPDIR)/core/stats.c \
+	$(LWIPDIR)/core/sys.c $(LWIPDIR)/core/tcp.c $(LWIPDIR)/core/tcp_in.c \
+	$(LWIPDIR)/core/tcp_out.c $(LWIPDIR)/core/udp.c $(LWIPDIR)/core/dhcp.c \
+	$(LWIPDIR)/core/init.c $(LWIPDIR)/core/timers.c $(LWIPDIR)/core/def.c
+
+CORE4FILES=$(wildcard $(LWIPDIR)/core/ipv4/*.c) $(LWIPDIR)/core/ipv4/inet.c \
+	$(LWIPDIR)/core/ipv4/inet_chksum.c
+
+# SNMPFILES: Extra SNMPv1 agent
+SNMPFILES=$(LWIPDIR)/core/snmp/asn1_dec.c $(LWIPDIR)/core/snmp/asn1_enc.c \
+	$(LWIPDIR)/core/snmp/mib2.c $(LWIPDIR)/core/snmp/mib_structs.c \
+	$(LWIPDIR)/core/snmp/msg_in.c $(LWIPDIR)/core/snmp/msg_out.c
+
+# APIFILES: The files which implement the sequential and socket APIs.
+APIFILES=$(LWIPDIR)/api/api_lib.c $(LWIPDIR)/api/api_msg.c $(LWIPDIR)/api/tcpip.c \
+	$(LWIPDIR)/api/err.c $(LWIPDIR)/api/sockets.c $(LWIPDIR)/api/netbuf.c $(LWIPDIR)/api/netdb.c
+
+# NETIFFILES: Files implementing various generic network interface functions.'
+NETIFFILES=$(LWIPDIR)/netif/etharp.c $(LWIPDIR)/netif/slipif.c
+
+# NETIFFILES: Add PPP netif
+NETIFFILES+=$(LWIPDIR)/netif/ppp/auth.c $(LWIPDIR)/netif/ppp/chap.c \
+	$(LWIPDIR)/netif/ppp/chpms.c $(LWIPDIR)/netif/ppp/fsm.c \
+	$(LWIPDIR)/netif/ppp/ipcp.c $(LWIPDIR)/netif/ppp/lcp.c \
+	$(LWIPDIR)/netif/ppp/magic.c $(LWIPDIR)/netif/ppp/md5.c \
+	$(LWIPDIR)/netif/ppp/pap.c $(LWIPDIR)/netif/ppp/ppp.c \
+	$(LWIPDIR)/netif/ppp/randm.c $(LWIPDIR)/netif/ppp/vj.c 
+
+LWIPPORTFILES = $(LWIPPORTDIR)/sys_arch.c
+
+#TCPIP source files
+LIBTCPIP_SRC = $(COREFILES) $(CORE4FILES) $(APIFILES) $(NETIFFILES) $(LWIPPORTFILES)
+
+#TCPIP include files
+LIBTCPIP_INC = -I$(LWIPDIR)/include -I$(LWIPDIR)/include/ipv4 -I$(LWIPPORTDIR)/include
+
+
+LIBTCPIP_LIB = $(LIBTCPIP_DIR)/libtcpip.a
+LIBTCPIP_OBJS += $(LIBTCPIP_SRC:%.c=%.o) $(LIBTCPIP_CPPSRC:%.cpp=%.o)
+DEPFILES += $(LIBTCPIP_SRC:%.c=%.dep)  $(LIBTCPIP_CPPSRC:%.cpp=%.dep)
+
+.ONESHELL:
+$(LIBTCPIP_LIB): $(LIBTCPIP_OBJS)
+	    $(AR) $(ARFLAGS) $@ $^
+
+LIBS += $(LIBTCPIP_LIB)
+LIBS_OBJS += $(LIBTCPIP_OBJS)
---
+++ libtcpip/port/sys_arch.c
@@ -0,0 +1,279 @@
+/* ------------------------------------------------------------------ */
+/* System architecture lwip for isix operating system
+ * Copyright (c) Lucjan Bryndza 2012
+ * All Rights reserverd
+ */
+/* ------------------------------------------------------------------ */
+#include "lwip/debug.h"
+#include "arch/sys_arch.h"
+#include "lwip/debug.h"
+#include "lwip/def.h"
+#include "lwip/sys.h"
+#include "lwip/mem.h"
+#include "lwip/timers.h"
+#include <isix.h>
+#include <dbglog.h>
+
+/* ------------------------------------------------------------------ */
+
+//Default task structure
+static struct sys_thread *threads = NULL;
+
+//Semaphore for locking the mempool
+static sem_t* arch_protect_sem;
+
+/* ------------------------------------------------------------------ */
+struct sys_thread
+{
+  struct sys_thread *next;
+  struct sys_timeo timeouts;
+  task_t *task;
+};
+
+/* ------------------------------------------------------------------ */
+//Create basic thread structure for LWIP
+static struct sys_thread* introduce_thread(task_t *task)
+{
+	struct sys_thread *thread;
+    thread = isix_alloc(sizeof(struct sys_thread));
+    if(thread)
+    {
+        thread->next = NULL;
+        thread->timeouts.next = NULL;
+        thread->task = task;
+    }
+    if(threads==NULL)
+    {
+        threads = thread;
+    }
+    else
+    {
+        struct sys_thread *st;
+        for(st=threads;st->next!=NULL;st=st->next);
+        st->next = thread;
+    }
+    return thread;
+}
+
+/* ------------------------------------------------------------------ */
+/* Return current thread with per process timeouts */
+static struct sys_thread * current_thread(void)
+{
+    struct sys_thread *st;
+    task_t *t;
+    t = isix_task_self();
+    for(st=threads;st!=NULL;st=st->next)
+    {
+        if(st->task==t) return st;
+    }
+    LWIP_ASSERT(st,"st==NULL");
+    return st;
+} 
+
+/* ------------------------------------------------------------------ */
+/** The only thread function:
+ * Creates a new thread
+ * @param name human-readable name for the thread (used for debugging purposes)
+ * @param thread thread-function
+ * @param arg parameter passed to 'thread'
+ * @param stacksize stack size in bytes for the new thread (may be ignored by ports)
+ * @param prio priority of the new thread (may be ignored by ports) */
+sys_thread_t sys_thread_new(const char *name, lwip_thread_fn thread, void *arg, int stacksize, int prio)
+{
+    (void)name;
+	struct sys_thread *st = NULL;
+    task_t* task = isix_task_create( thread, arg, stacksize, prio);
+    if(task != NULL )
+    {
+        st = introduce_thread(task);
+    }
+    dbprintf("sys_thread_new: New thread created %08x\n",task);
+    return st;
+}
+
+
+/* ------------------------------------------------------------------ */
+/** Create a new semaphore
+ * @param sem pointer to the semaphore to create
+ * @param count initial count of the semaphore
+ * @return ERR_OK if successful, another err_t otherwise */
+err_t sys_sem_new(sys_sem_t *sem, u8_t count)
+{
+    *sem = isix_sem_create(NULL,count);
+    return (*sem)?(ERR_OK):(ERR_MEM);
+}
+
+
+/* ------------------------------------------------------------------ */
+/** Delete a semaphore
+ * @param sem semaphore to delete */
+void sys_sem_free(sys_sem_t *sem)
+{
+    isix_sem_destroy(*sem);
+}
+
+/* ------------------------------------------------------------------ */
+/** Signals a semaphore
+ * @param sem the semaphore to signal */
+void sys_sem_signal(sys_sem_t *sem)
+{
+    isix_sem_signal(*sem);
+}
+
+/* ------------------------------------------------------------------ */
+/** Wait for a semaphore for the specified timeout
+ * @param sem the semaphore to wait for
+ * @param timeout timeout in milliseconds to wait (0 = wait forever)
+ * @return time (in milliseconds) waited for the semaphore
+ *         or SYS_ARCH_TIMEOUT on timeout */
+u32_t sys_arch_sem_wait(sys_sem_t *sem, u32_t timeout)
+{
+    timeout = (timeout*ISIX_HZ)/1000;
+    tick_t t = isix_get_jiffies();
+    int reason = isix_sem_wait(*sem,timeout);
+    t =  isix_get_jiffies() - t;
+    t = (t*1000)/ISIX_HZ;
+    if(reason == ISIX_ETIMEOUT) return SYS_ARCH_TIMEOUT;
+    return t;
+}
+
+
+/* ------------------------------------------------------------------ */
+/*
+  Returns a pointer to the per-thread sys_timeouts structure. In lwIP,
+  each thread has a list of timeouts which is represented as a linked
+  list of sys_timeout structures. The sys_timeouts structure holds a
+  pointer to a linked list of timeouts. This function is called by
+  the lwIP timeout scheduler and must not return a NULL value.
+
+  In a single threaded sys_arch implementation, this function will
+  simply return a pointer to a global sys_timeouts variable stored in
+  the sys_arch module.
+*/
+struct sys_timeo* sys_arch_timeouts(void)
+{
+    struct sys_thread *thread;
+    thread = current_thread();
+    if(thread==NULL) return NULL;
+    return &thread->timeouts;
+}
+/* ------------------------------------------------------------------ */
+/*
+Blocks the thread until a message arrives in the mailbox, but does
+not block the thread longer than "timeout" milliseconds (similar to
+the sys_arch_sem_wait() function). The "msg" argument is a result
+parameter that is set by the function (i.e., by doing "*msg =
+ptr"). The "msg" parameter maybe NULL to indicate that the message
+should be dropped.
+
+The return values are the same as for the sys_arch_sem_wait() function:
+Number of milliseconds spent waiting or SYS_ARCH_TIMEOUT if there was a
+timeout.
+
+Note that a function with a similar name, sys_mbox_fetch(), is
+implemented by lwIP.
+*/
+u32_t sys_arch_mbox_fetch(sys_mbox_t *mbox, void **msg, u32_t timeout)
+{
+    void *fifor=NULL;
+    timeout = (timeout*ISIX_HZ)/1000;
+    tick_t t = isix_get_jiffies();
+    int reason = isix_fifo_read(*mbox,&fifor,timeout);
+    t = isix_get_jiffies() - t;
+    t = (t*1000)/ISIX_HZ;
+    if(reason == ISIX_ETIMEOUT) return SYS_ARCH_TIMEOUT;
+    if(msg != NULL) *msg = fifor;
+    return t;
+}
+
+/* ------------------------------------------------------------------ */
+/*
+  Deallocates a mailbox. If there are messages still present in the
+  mailbox when the mailbox is deallocated, it is an indication of a
+  programming error in lwIP and the developer should be notified.
+*/
+void sys_mbox_free(sys_mbox_t *mbox)
+{
+    isix_fifo_destroy(*mbox);
+}
+
+/* ------------------------------------------------------------------ */
+/** Create a new mbox of specified size
+ * @param mbox pointer to the mbox to create
+ * @param size (miminum) number of messages in this mbox
+ * @return ERR_OK if successful, another err_t otherwise */
+err_t sys_mbox_new(sys_mbox_t *mbox, int size)
+{
+   *mbox = isix_fifo_create(size,sizeof(void*));
+   return (*mbox)?(ERR_OK):(ERR_MEM);
+}
+/* ------------------------------------------------------------------ */
+/* Post the message to the mailbox */
+void sys_mbox_post(sys_mbox_t *mbox, void *msg)
+{
+    void *tmp = msg;
+   isix_fifo_write(*mbox,&tmp,0);
+}
+/* ------------------------------------------------------------------ */
+/** Wait for a new message to arrive in the mbox
+ * @param mbox mbox to get a message from
+ * @param msg pointer where the message is stored
+ * @param timeout maximum time (in milliseconds) to wait for a message
+ * @return 0 (milliseconds) if a message has been received
+ *         or SYS_MBOX_EMPTY if the mailbox is empty */
+u32_t sys_arch_mbox_tryfetch(sys_mbox_t *mbox, void **msg)
+{
+	int reason = isix_fifo_read_isr( *mbox, msg );
+	return ( reason==ISIX_EOK )?( 0 ):( SYS_MBOX_EMPTY );
+}
+
+/* ------------------------------------------------------------------ */
+/** Try to post a message to an mbox - may fail if full or ISR
+ * @param mbox mbox to posts the message
+ * @param msg message to post (ATTENTION: can be NULL) */
+err_t sys_mbox_trypost(sys_mbox_t *mbox, void *msg)
+{
+	int reason = isix_fifo_write_isr( *mbox, msg );
+	return ( reason==ISIX_EOK )?( 0 ):( SYS_MBOX_EMPTY );
+}
+
+/* ------------------------------------------------------------------ */
+/*
+ * Perform a "fast" protect. This could be implemented by
+ * disabling interrupts for an embedded system or by using a semaphore or
+ * mutex. The implementation should allow calling SYS_ARCH_PROTECT when
+ * already protected. The old protection level is returned in the variable
+ * "lev". This macro will default to calling the sys_arch_protect() function
+ * which should be implemented in sys_arch.c. If a particular port needs a
+ * different implementation, then this macro may be defined in sys_arch.h
+*/
+/* ------------------------------------------------------------------ */
+sys_prot_t sys_arch_protect(void)
+{
+    isix_sem_wait( arch_protect_sem, ISIX_TIME_INFINITE );
+	return 1;
+}
+/*-----------------------------------------------------------------------------------*/
+/** void sys_arch_unprotect(sys_prot_t pval)
+
+This optional function does a "fast" set of critical region protection to the
+value specified by pval. See the documentation for sys_arch_protect() for
+more information. This function is only required if your port is supporting
+an operating system.
+*/
+void sys_arch_unprotect(sys_prot_t pval)
+{
+	(void)pval;
+	isix_sem_signal( arch_protect_sem );
+}
+/* ------------------------------------------------------------------ */
+/* Add system initialize stuff before tcpip init */
+void sys_init(void)
+{
+	arch_protect_sem = isix_sem_create( NULL, 0 );
+	LWIP_ASSERT("Unable to create lock semaphore", arch_protect_sem );
+}
+
+/* ------------------------------------------------------------------ */
+
+
