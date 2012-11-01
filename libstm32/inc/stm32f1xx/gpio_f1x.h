9f8700dbfb490966cd785fa4bf2fd26a94daebf0
Lucjan Bryndza <lbryndza.p@boff.pl>
2012-11-01 16:20:33 +0100

    Remap function in the stm32 lib

--- libstm32/inc/stm32f1xx/gpio_f1x.h
+++ libstm32/inc/stm32f1xx/gpio_f1x.h
@@ -17,6 +17,121 @@
 namespace stm32 {
 #endif
 /*----------------------------------------------------------*/
+#ifdef __cplusplus
+static const uint32_t GPIO_Remap_SPI1             = 0x00000001;
+static const uint32_t GPIO_Remap_I2C1             = 0x00000002;
+static const uint32_t GPIO_Remap_USART1           = 0x00000004;
+static const uint32_t GPIO_Remap_USART2           = 0x00000008;
+static const uint32_t GPIO_PartialRemap_USART3    = 0x00140010;
+static const uint32_t GPIO_FullRemap_USART3       = 0x00140030;
+static const uint32_t GPIO_PartialRemap_TIM1      = 0x00160040;
+static const uint32_t GPIO_FullRemap_TIM1         = 0x001600C0;
+static const uint32_t GPIO_PartialRemap1_TIM2     = 0x00180100;
+static const uint32_t GPIO_PartialRemap2_TIM2     = 0x00180200;
+static const uint32_t GPIO_FullRemap_TIM2         = 0x00180300;
+static const uint32_t GPIO_PartialRemap_TIM3      = 0x001A0800;
+static const uint32_t GPIO_FullRemap_TIM3         = 0x001A0C00;
+static const uint32_t GPIO_Remap_TIM4             = 0x00001000;
+static const uint32_t GPIO_Remap1_CAN1            = 0x001D4000;
+static const uint32_t GPIO_Remap2_CAN1            = 0x001D6000;
+static const uint32_t GPIO_Remap_PD01             = 0x00008000;
+static const uint32_t GPIO_Remap_TIM5CH4_LSI      = 0x00200001;
+static const uint32_t GPIO_Remap_ADC1_ETRGINJ     = 0x00200002;
+static const uint32_t GPIO_Remap_ADC1_ETRGREG     = 0x00200004;
+static const uint32_t GPIO_Remap_ADC2_ETRGINJ     = 0x00200008;
+static const uint32_t GPIO_Remap_ADC2_ETRGREG     = 0x00200010;
+static const uint32_t GPIO_Remap_ETH              = 0x00200020;
+static const uint32_t GPIO_Remap_CAN2             = 0x00200040;
+static const uint32_t GPIO_Remap_SWJ_NoJTRST      = 0x00300100;
+static const uint32_t GPIO_Remap_SWJ_JTAGDisable  = 0x00300200;
+static const uint32_t GPIO_Remap_SWJ_Disable      = 0x00300400;
+static const uint32_t GPIO_Remap_SPI3             = 0x00201100;
+static const uint32_t GPIO_Remap_TIM2ITR1_PTP_SOF = 0x00202000;
+static const uint32_t GPIO_Remap_PTP_PPS          = 0x00204000;
+static const uint32_t GPIO_Remap_TIM15            = 0x80000001;
+static const uint32_t GPIO_Remap_TIM16            = 0x80000002;
+static const uint32_t GPIO_Remap_TIM17            = 0x80000004;
+static const uint32_t GPIO_Remap_CEC              = 0x80000008;
+static const uint32_t GPIO_Remap_TIM1_DMA         = 0x80000010;
+static const uint32_t GPIO_Remap_TIM9             = 0x80000020;
+static const uint32_t GPIO_Remap_TIM10            = 0x80000040;
+static const uint32_t GPIO_Remap_TIM11            = 0x80000080;
+static const uint32_t GPIO_Remap_TIM13            = 0x80000100;
+static const uint32_t GPIO_Remap_TIM14            = 0x80000200;
+static const uint32_t GPIO_Remap_FSMC_NADV        = 0x80000400;
+static const uint32_t GPIO_Remap_TIM67_DAC_DMA    = 0x80000800;
+static const uint32_t GPIO_Remap_TIM12            = 0x80001000;
+static const uint32_t GPIO_Remap_MISC             = 0x80002000;
+
+#else
+
+#define GPIO_Remap_SPI1             ((uint32_t)0x00000001)  /*!< SPI1 Alternate Function mapping */
+#define GPIO_Remap_I2C1             ((uint32_t)0x00000002)  /*!< I2C1 Alternate Function mapping */
+#define GPIO_Remap_USART1           ((uint32_t)0x00000004)  /*!< USART1 Alternate Function mapping */
+#define GPIO_Remap_USART2           ((uint32_t)0x00000008)  /*!< USART2 Alternate Function mapping */
+#define GPIO_PartialRemap_USART3    ((uint32_t)0x00140010)  /*!< USART3 Partial Alternate Function mapping */
+#define GPIO_FullRemap_USART3       ((uint32_t)0x00140030)  /*!< USART3 Full Alternate Function mapping */
+#define GPIO_PartialRemap_TIM1      ((uint32_t)0x00160040)  /*!< TIM1 Partial Alternate Function mapping */
+#define GPIO_FullRemap_TIM1         ((uint32_t)0x001600C0)  /*!< TIM1 Full Alternate Function mapping */
+#define GPIO_PartialRemap1_TIM2     ((uint32_t)0x00180100)  /*!< TIM2 Partial1 Alternate Function mapping */
+#define GPIO_PartialRemap2_TIM2     ((uint32_t)0x00180200)  /*!< TIM2 Partial2 Alternate Function mapping */
+#define GPIO_FullRemap_TIM2         ((uint32_t)0x00180300)  /*!< TIM2 Full Alternate Function mapping */
+#define GPIO_PartialRemap_TIM3      ((uint32_t)0x001A0800)  /*!< TIM3 Partial Alternate Function mapping */
+#define GPIO_FullRemap_TIM3         ((uint32_t)0x001A0C00)  /*!< TIM3 Full Alternate Function mapping */
+#define GPIO_Remap_TIM4             ((uint32_t)0x00001000)  /*!< TIM4 Alternate Function mapping */
+#define GPIO_Remap1_CAN1            ((uint32_t)0x001D4000)  /*!< CAN1 Alternate Function mapping */
+#define GPIO_Remap2_CAN1            ((uint32_t)0x001D6000)  /*!< CAN1 Alternate Function mapping */
+#define GPIO_Remap_PD01             ((uint32_t)0x00008000)  /*!< PD01 Alternate Function mapping */
+#define GPIO_Remap_TIM5CH4_LSI      ((uint32_t)0x00200001)  /*!< LSI connected to TIM5 Channel4 input capture for calibration */
+#define GPIO_Remap_ADC1_ETRGINJ     ((uint32_t)0x00200002)  /*!< ADC1 External Trigger Injected Conversion remapping */
+#define GPIO_Remap_ADC1_ETRGREG     ((uint32_t)0x00200004)  /*!< ADC1 External Trigger Regular Conversion remapping */
+#define GPIO_Remap_ADC2_ETRGINJ     ((uint32_t)0x00200008)  /*!< ADC2 External Trigger Injected Conversion remapping */
+#define GPIO_Remap_ADC2_ETRGREG     ((uint32_t)0x00200010)  /*!< ADC2 External Trigger Regular Conversion remapping */
+#define GPIO_Remap_ETH              ((uint32_t)0x00200020)  /*!< Ethernet remapping (only for Connectivity line devices) */
+#define GPIO_Remap_CAN2             ((uint32_t)0x00200040)  /*!< CAN2 remapping (only for Connectivity line devices) */
+#define GPIO_Remap_SWJ_NoJTRST      ((uint32_t)0x00300100)  /*!< Full SWJ Enabled (JTAG-DP + SW-DP) but without JTRST */
+#define GPIO_Remap_SWJ_JTAGDisable  ((uint32_t)0x00300200)  /*!< JTAG-DP Disabled and SW-DP Enabled */
+#define GPIO_Remap_SWJ_Disable      ((uint32_t)0x00300400)  /*!< Full SWJ Disabled (JTAG-DP + SW-DP) */
+#define GPIO_Remap_SPI3             ((uint32_t)0x00201100)  /*!< SPI3/I2S3 Alternate Function mapping (only for Connectivity line devices) */
+#define GPIO_Remap_TIM2ITR1_PTP_SOF ((uint32_t)0x00202000)  /*!< Ethernet PTP output or USB OTG SOF (Start of Frame) connected                                                               to TIM2 Internal Trigger 1 for calibration                                                                (only for Connectivity line devices) */
+#define GPIO_Remap_PTP_PPS          ((uint32_t)0x00204000)  /*!< Ethernet MAC PPS_PTS output on PB05 (only for Connectivity line devices) */
+#define GPIO_Remap_TIM15            ((uint32_t)0x80000001)  /*!< TIM15 Alternate Function mapping (only for Value line devices) */
+#define GPIO_Remap_TIM16            ((uint32_t)0x80000002)  /*!< TIM16 Alternate Function mapping (only for Value line devices) */
+#define GPIO_Remap_TIM17            ((uint32_t)0x80000004)  /*!< TIM17 Alternate Function mapping (only for Value line devices) */
+#define GPIO_Remap_CEC              ((uint32_t)0x80000008)  /*!< CEC Alternate Function mapping (only for Value line devices) */
+#define GPIO_Remap_TIM1_DMA         ((uint32_t)0x80000010)  /*!< TIM1 DMA requests mapping (only for Value line devices) */
+#define GPIO_Remap_TIM9             ((uint32_t)0x80000020)  /*!< TIM9 Alternate Function mapping (only for XL-density devices) */
+#define GPIO_Remap_TIM10            ((uint32_t)0x80000040)  /*!< TIM10 Alternate Function mapping (only for XL-density devices) */
+#define GPIO_Remap_TIM11            ((uint32_t)0x80000080)  /*!< TIM11 Alternate Function mapping (only for XL-density devices) */
+#define GPIO_Remap_TIM13            ((uint32_t)0x80000100)  /*!< TIM13 Alternate Function mapping (only for High density Value line and XL-density devices) */
+#define GPIO_Remap_TIM14            ((uint32_t)0x80000200)  /*!< TIM14 Alternate Function mapping (only for High density Value line and XL-density devices) */
+#define GPIO_Remap_FSMC_NADV        ((uint32_t)0x80000400)  /*!< FSMC_NADV Alternate Function mapping (only for High density Value line and XL-density devices) */
+#define GPIO_Remap_TIM67_DAC_DMA    ((uint32_t)0x80000800)  /*!< TIM6/TIM7 and DAC DMA requests remapping (only for High density Value line devices) */
+#define GPIO_Remap_TIM12            ((uint32_t)0x80001000)  /*!< TIM12 Alternate Function mapping (only for High density Value line devices) */
+#define GPIO_Remap_MISC             ((uint32_t)0x80002000)  /*!< Miscellaneous Remap (DMA2 Channel5 Position and DAC Trigger remapping, only for High density Value line devices) */
+
+#endif
+/*----------------------------------------------------------*/
+
+#ifdef __cplusplus
+namespace _internal {
+namespace gpio {
+static const uint16_t EVCR_PORTPINCONFIG_MASK  =  0xFF80;
+static const uint16_t LSB_MASK                 =  0xFFFF;
+static const uint32_t DBGAFR_POSITION_MASK     =  0x000F0000;
+static const uint32_t DBGAFR_SWJCFG_MASK       =  0xF0FFFFFF;
+static const uint32_t DBGAFR_LOCATION_MASK     =  0x00200000;
+static const uint32_t DBGAFR_NUMBITS_MASK      =  0x00100000;
+}}
+#else
+#define EVCR_PORTPINCONFIG_MASK     ((uint16_t)0xFF80)
+#define LSB_MASK                    ((uint16_t)0xFFFF)
+#define DBGAFR_POSITION_MASK        ((uint32_t)0x000F0000)
+#define DBGAFR_SWJCFG_MASK          ((uint32_t)0xF0FFFFFF)
+#define DBGAFR_LOCATION_MASK        ((uint32_t)0x00200000)
+#define DBGAFR_NUMBITS_MASK         ((uint32_t)0x00100000)
+#endif
+/*----------------------------------------------------------*/
 
 /** GPIO bits macros */
 enum e_gpio_mode
@@ -221,6 +336,61 @@ static inline void gpio_clock_enable( GPIO_TypeDef* port, bool enable )
 	else
 		RCC->APB2ENR &=  ~_gpio_clock_port_to_number( port );
 }
+/*----------------------------------------------------------*/
+/* Remap pin to the selected perhipheral */
+
+static inline void gpio_pin_remap_config( uint32_t GPIO_Remap, bool enable )
+{
+#ifdef __cplusplus
+	using namespace _internal::gpio;
+ #endif
+
+  uint32_t tmp = 0x00, tmp1 = 0x00, tmpreg = 0x00, tmpmask = 0x00;
+
+  if((GPIO_Remap & 0x80000000) == 0x80000000)
+  {
+    tmpreg = AFIO->MAPR2;
+  }
+  else
+  {
+    tmpreg = AFIO->MAPR;
+  }
+
+  tmpmask = (GPIO_Remap & DBGAFR_POSITION_MASK) >> 0x10;
+  tmp = GPIO_Remap & LSB_MASK;
+
+  if ((GPIO_Remap & (DBGAFR_LOCATION_MASK | DBGAFR_NUMBITS_MASK)) == (DBGAFR_LOCATION_MASK | DBGAFR_NUMBITS_MASK))
+  {
+    tmpreg &= DBGAFR_SWJCFG_MASK;
+    AFIO->MAPR &= DBGAFR_SWJCFG_MASK;
+  }
+  else if ((GPIO_Remap & DBGAFR_NUMBITS_MASK) == DBGAFR_NUMBITS_MASK)
+  {
+    tmp1 = ((uint32_t)0x03) << tmpmask;
+    tmpreg &= ~tmp1;
+    tmpreg |= ~DBGAFR_SWJCFG_MASK;
+  }
+  else
+  {
+    tmpreg &= ~(tmp << ((GPIO_Remap >> 0x15)*0x10));
+    tmpreg |= ~DBGAFR_SWJCFG_MASK;
+  }
+
+  if ( enable )
+  {
+    tmpreg |= (tmp << ((GPIO_Remap >> 0x15)*0x10));
+  }
+
+  if((GPIO_Remap & 0x80000000) == 0x80000000)
+  {
+    AFIO->MAPR2 = tmpreg;
+  }
+  else
+  {
+    AFIO->MAPR = tmpreg;
+  }
+}
+
 /*----------------------------------------------------------*/
 #ifdef __cplusplus
 }
