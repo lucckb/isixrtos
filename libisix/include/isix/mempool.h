61e02810170acebf2da09dccce7e0b3989370057
Lucjan Bryndza <lbryndza.p@boff.pl>
2012-11-17 13:17:03 +0100

    Added c++ mempool

--- libisix/include/isix/mempool.h
+++ libisix/include/isix/mempool.h
@@ -50,11 +50,60 @@ void* isix_mempool_alloc( isix_mempool_t mp );
  * @param[in] p memory from mempool
  */
 void isix_mempool_free( isix_mempool_t mp, void *p );
-
 /*-----------------------------------------------------------------------*/
 #ifdef __cplusplus
 }	//end namespace
 }	//end extern-C
+#endif /* __cplusplus */
+/*-----------------------------------------------------------------------*/
+#ifdef __cplusplus
+#include <cstddef>
+/*-----------------------------------------------------------------------*/
+namespace isix {
+/*-----------------------------------------------------------------------*/
+/** Mempool class for pool memory management
+ *  WARNING! This class is indented only for
+ *  usage with POD types, and it is helper class only.
+ */
+template <typename T> class mempool
+{
+public:
+	/** Constructor
+	 * @param[in] elems Number of elements
+	 */
+	explicit mempool( size_t elems )
+		: m_mp( isix_mempool_create( elems, sizeof(T) ) )
+	{
+	}
+	//! Destructor
+	~mempool()
+	{
+		isix_mempool_destroy( m_mp );
+	}
+	/** Alloc mempool element
+	 * @return allocated element
+	 */
+	T* alloc()
+	{
+		return static_cast<T*>( isix_mempool_alloc(m_mp) );
+	}
+	void free( T* p )
+	{
+		isix_mempool_free( m_mp, p );
+	}
+	/** Check if the fifo object is in valid state
+	 * @return true if object is ok else return false
+	 */
+	bool is_valid() { return m_mp!=0; }
+private:
+	isix_mempool_t m_mp;
+private:	//Noncopyable
+	mempool(const mempool&);
+	mempool& operator=(const mempool&);
+};
+/*-----------------------------------------------------------------------*/
+}
+/*-----------------------------------------------------------------------*/
 #endif /* __cplusplus */
 /*-----------------------------------------------------------------------*/
 #endif /* MEMPOOL_H_ */
