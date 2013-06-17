05978638f827408a73c9e73c3a711892ea6bd923
Lucjan Bryndza <lbryndza.p@boff.pl>
2013-06-17 13:23:35 +0200

    Host tool changed

--- libgfx/host-tools/convflashbmp.cpp
+++ libgfx/host-tools/convflashbmp.cpp
@@ -4,39 +4,134 @@
  *  Created on: 16-06-2013
  *      Author: lucck
  */
-#include <CImg.h>
+
 #include <cstddef>
 #include <iostream>
+#include <IL/il.h>
+#include <IL/ilu.h>
+#include <stdexcept>
+#include "lz.h"
+/* ----------------------------------------------------------------------------- */
+inline void il_abort( ILboolean error )
+{
+    if( !error )
+    {
+        std::cerr << ilGetError() << std::endl;
+        throw std::runtime_error("IL error");
+    }
+}
 
-
+/* ----------------------------------------------------------------------------- */
 typedef unsigned short color_t;
 static inline constexpr color_t rgb( unsigned char R, unsigned char G, unsigned char B )
 {
 		return ((B>>3)<<(16-5)) | ((G>>2)<<(16-5-6) | (R>>3) );
 }
 
+/* ----------------------------------------------------------------------------- */
+class pixel
+{
+public:
+    pixel( unsigned char * buf )
+      : m_data(buf)
+      {}
+      unsigned char R() const
+      {
+        return m_data[0];
+      }
+      unsigned char G() const
+      {
+        return m_data[1];
+      }
+      unsigned char B() const
+      {
+        return m_data[2];
+      }
+      void R( unsigned char v )
+      {
+        m_data[0] = v;
+      }
+      void G( unsigned char v )
+      {
+        m_data[1] = v;
+      }
+      void B( unsigned char v )
+      {
+        m_data[2] = v;
+      }
+      operator color_t() const
+      {
+         return rgb( R(), G(), B() );
+      }
+private:
+    unsigned char *m_data;
+};
+
+class img_row
+{
+public:
+    img_row( unsigned char *row )
+        : m_data( row )
+    {}
+    pixel operator[](size_t index)
+    {
+        return pixel(&m_data[index*3]);
+    }
+private:
+    unsigned char *m_data;
+};
+
+class img_array
+{
+public:
+    img_array()
+    {
+        m_width = ilGetInteger(IL_IMAGE_WIDTH);
+        const int height = ilGetInteger(IL_IMAGE_HEIGHT);
+        m_data = new unsigned char[ m_width * height * 3 ];
+        ilCopyPixels( 0, 0, 0, m_width, height, 1, IL_RGB, IL_UNSIGNED_BYTE ,m_data  );
+    }
+    img_row operator[](size_t index)
+    {
+        return img_row( &m_data[ index * m_width *3 ] );
+    }
+    ~img_array()
+    {
+        delete [] m_data;
+    }
+private:
+    unsigned char *m_data;
+    int m_width;
+};
+
+/* ----------------------------------------------------------------------------- */
 int main()
 {
-	using namespace cimg_library;
-	CImg<unsigned char> image("/tmp/mkeia.jpg");
-	const auto width = image.width();
-	const auto height = image.height();
-	color_t *bmp = new color_t[width*height*sizeof(color_t)];
-	color_t compressed[256];
-	for( size_t x=0; x<width; ++x)
-	for( size_t y=0,lp=0; y<height; ++lp,++y )
-	{
-		const auto R = image[image.offset( x,y,0,0 )];
-		const auto G = image[image.offset( x,y,0,1 )];
-		const auto B = image[image.offset( x,y,0,2 )];
-		const auto color = rgb(R,G,B);
-		bmp[lp] = color;
-		if( lp == 127 )
-		{
-			//std::cout << Rice_Compress(bmp,compressed,lp*sizeof(color_t), RICE_FMT_INT16) << std::endl;
-			lp = 0;
-		}
-		//std::cout << color << std::endl;
-	}
-	return 0;
+	/* Init library */
+    ilInit();
+    iluInit();
+    /* Load image */
+    il_abort( ilLoadImage("/tmp/mkeia.jpg") );
+    const int width = ilGetInteger(IL_IMAGE_WIDTH);
+    const int height = ilGetInteger(IL_IMAGE_HEIGHT);
+    std::cout << "Img size " << width << "x" << height << std::endl;
+    img_array img;
+    int cl = 0;
+    color_t line[128];
+    color_t linec[1024];
+    size_t ll = 0;
+    for(int y=0; y<height; ++y)
+    for(int x=0; x<width; ++x)
+    {
+        line[cl++] = img[y][x];
+        //std::cout << int(line[cl-1]) << " V "<< int(img[y][x]) << std::endl;
+        if( cl == 128 )
+        {
+            const int clen = LZ_Compress((unsigned char*)line, (unsigned char*)linec, 256 );
+            cl = 0;
+            ll += clen + 1;
+        }
+    }
+    std::cout << (width*height*2) << " " << ll << std::endl;
+   	return 0;
 }
