28cbcaa605bc320bb5647a54f4b4d4bc9f2314e1
Lucjan Bryndza <lbryndza.p@boff.pl>
2013-10-02 15:44:20 +0200

    sync

---
+++ libgfx/src/gui/button.cpp
@@ -0,0 +1,31 @@
+/*
+ * button.cpp
+ *
+ *  Created on: 2 paź 2013
+ *      Author: lucck
+ */
+/* ------------------------------------------------------------------ */
+#include <gfx/gui/button.hpp>
+#include <foundation/dbglog.h>
+/* ------------------------------------------------------------------ */
+namespace gfx {
+namespace gui {
+
+/* ------------------------------------------------------------------ */
+// On repaint the window return true when changed
+bool button::repaint()
+{
+	gdi().fill_area(get_x(), get_y(), get_cx(), get_y() );
+	//gdi().draw_text(get_x(), get_y(), m_caption.c_str());
+}
+/* ------------------------------------------------------------------ */
+//* Report input event
+bool button::report_event( const input::event_info& ev )
+{
+	dbprintf("report_event");
+}
+/* ------------------------------------------------------------------ */
+
+} /* namespace gui */
+} /* namespace gfx */
+/* ------------------------------------------------------------------ */
