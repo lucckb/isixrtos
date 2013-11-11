a12f8b010f33376b9fda4a422c2b00f03c322de8
Lucjan Bryndza <lbryndza.p@boff.pl>
2013-11-11 19:38:18 +0100

    Operator for compare icon sizes

--- libfoundation/include/foundation/lcd_bitmap.hpp
+++ libfoundation/include/foundation/lcd_bitmap.hpp
@@ -19,7 +19,18 @@ struct icon_t
 	unsigned char height;
 	const unsigned char* data;
 };
-
+/* ------------------------------------------------------------------ */
+//! Operator for determine the icon has diffrent size
+static inline bool operator!=( const icon_t& i1, const icon_t& i2 )
+{
+	return (i1.pg_width != i2.pg_width) || (i1.height != i2.height );
+}
+/* ------------------------------------------------------------------ */
+//! Operator for determine the icon has diffrent size
+static inline bool operator==( const icon_t& i1, const icon_t& i2 )
+{
+	return (i1.pg_width == i2.pg_width) && (i1.height == i2.height );
+}
 /* ------------------------------------------------------------------ */
 }}
 /* ------------------------------------------------------------------ */
