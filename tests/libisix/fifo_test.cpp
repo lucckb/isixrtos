e0d3a374172b8b4d6b2304b0036081144ff03219
Lucjan Bryndza <lbryndza.p@boff.pl>
2013-12-12 21:14:34 +0100

    New sema test updated

--- tests/libisix/fifo_test.cpp
+++ tests/libisix/fifo_test.cpp
@@ -20,7 +20,7 @@
 #include "fifo_test.hpp"
 #include <isix.h>
 #include <qunit.hpp>
-
+#include <foundation/dbglog.h>
 
 namespace tests {
 
@@ -37,6 +37,7 @@ namespace {
             m_fifo( fifo ), m_id( ch_id )
 		{
 		}
+		virtual ~task_test() {}
 	protected:
 		//Main function from another task
 		virtual void main( void ) 
@@ -62,24 +63,34 @@ void fifo_test::base_tests()
 	QUNIT_IS_EQUAL( isix::isix_task_change_prio( nullptr, test_prio ), TASKDEF_PRIORITY );	        
 	isix::fifo<char> fifo_tst( 32 );
 	QUNIT_IS_TRUE( fifo_tst.is_valid() );  
+	  
+	task_test* t1 = new task_test( 'A',  3, fifo_tst );
+	task_test* t2 = new task_test( 'B',  2, fifo_tst );
+	task_test* t3 = new task_test( 'C',  1, fifo_tst );
+	task_test* t4 = new task_test( 'D',  0, fifo_tst );
+	/*	
 	task_test t1( 'A',  3, fifo_tst );
 	task_test t2( 'B',  2, fifo_tst );
 	task_test t3( 'C',  1, fifo_tst );
 	task_test t4( 'D',  0, fifo_tst );
+	 */
 	char tbuf[17] {};
 	size_t tbcnt {};
 	int err;
 	char ch;
-	QUNIT_IS_EQUAL( isix::isix_task_change_prio(nullptr,TASKDEF_PRIORITY ), test_prio );
 	for( tbcnt=0; (err=fifo_tst.pop( ch, 1000 ))==isix::ISIX_EOK; tbuf[tbcnt++]=ch );
 	QUNIT_IS_EQUAL( err, isix::ISIX_ETIMEOUT );
 	QUNIT_IS_EQUAL( tbcnt, task_test::N_ITEMS * 4 );
 	QUNIT_IS_FALSE( std::strcmp(tbuf, "DDDDCCCCBBBBAAAA") );
+	QUNIT_IS_EQUAL( isix::isix_task_change_prio(nullptr,TASKDEF_PRIORITY ), test_prio );
+	
+	 delete t1; delete t2; delete t3; delete t4;
 }
 
 //Overflow semaphore test
 void fifo_test::insert_overflow() 
 {	
+	///isix::isix_wait_ms(15);
 	static constexpr auto FIFO_SIZE = 64;
 	isix::fifo<char> ovfifo( FIFO_SIZE );
 	QUNIT_IS_TRUE( ovfifo.is_valid() );
@@ -91,7 +102,9 @@ void fifo_test::insert_overflow()
 	}
 	QUNIT_IS_EQUAL( err, isix::ISIX_EOK );
 	QUNIT_IS_EQUAL( ovfifo.size(), FIFO_SIZE );
-	QUNIT_IS_EQUAL( ovfifo.push('X'), isix::ISIX_EFIFOFULL );
-}
+	QUNIT_IS_EQUAL( ovfifo.push('X',1000), isix::ISIX_ETIMEOUT );
+	QUNIT_IS_EQUAL( ovfifo.push('X',1000), isix::ISIX_ETIMEOUT );
+ 
+}  
 
-} //Test Ns end
+}//Test Ns end
