a003ed3c4605700288d91ee593e8c1f41404dd85
Lucjan Bryndza <lbryndza.p@boff.pl>
2014-01-02 14:55:21 +0100

    Warning about multiobject

--- libisix/include/isix/config.h
+++ libisix/include/isix/config.h
@@ -16,4 +16,8 @@
 #error Shutdown API ISIX_CONFIG_SHUTDOWN_API require main crt exit code FUNCTION_MAIN_RETURN
 #endif
 
+#if defined(ISIX_CONFIG_USE_MULTIOBJECTS)
+#error ISIX_CONFIG_USE_MULTIOBJECTS multiobjects not implemented yet
+#endif
+
 #endif
