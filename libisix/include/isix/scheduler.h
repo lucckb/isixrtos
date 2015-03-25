10d42c4e59a4ea3b28c3f6c2b4b8d108032df7c0
Lucjan Bryndza <lbryndza.p@boff.pl>
2015-03-25 17:54:39 +0100

    Isix wait ujiffies

--- libisix/include/isix/scheduler.h
+++ libisix/include/isix/scheduler.h
@@ -103,6 +103,21 @@ static inline utick_t isix_get_ujiffies(void)
     return t;
 }
 /* ----------------------------------------------------------------------*/
+/** Busy waiting for selecred amount of time
+ * @param[in] timeout Number of microseconds for busy wait
+ * @return None
+ */
+static inline void isix_wait_us( unsigned timeout )
+{
+	utick_t t1 = isix_get_ujiffies();
+	for(;;) 
+	{
+		utick_t t2 =  isix_get_ujiffies();
+		if( t2>=t1 ) { if( t2-t1>timeout ) break; }
+		else { if( t1-t2>timeout) break; }
+	}
+}
+/* ----------------------------------------------------------------------*/
 /** Check if isix timer elapsed or not
  *   @param[in] t1 Variable with the initial timer value
      @param[in] timeout Excepted timeout 
@@ -111,7 +126,7 @@ static inline utick_t isix_get_ujiffies(void)
 static inline bool isix_timer_elapsed( tick_t t1, tick_t timeout )
 {
 	tick_t t2 = isix_get_jiffies();
-	if( t2 >= t1) 	//Not overflow
+	if( t2 >= t1 ) 	//Not overflow
 		return t2 - t1 > timeout;
 	else   	       //Overflow
 		return t1 - t2 > timeout;
