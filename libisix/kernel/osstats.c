c73552a2fd84415855caa0947ad307e09e1c3cd1
Lucjan Bryndza <lbryndza.p@boff.pl>
2015-05-15 21:26:49 +0200

    small fix

--- libisix/kernel/osstats.c
+++ libisix/kernel/osstats.c
@@ -31,14 +31,14 @@
 
 
 //! Calculate nearst power of two 
-#define __LOG2A(s) (((s) &0xffffffff00000000) ? (32 +_LOG2B((s) >>32)): (_LOG2B(s)))
+#define _LOG2A(s) (((s) &0xffffffff00000000) ? (32 +_LOG2B((s) >>32)): (_LOG2B(s)))
 #define _LOG2B(s) (((s) &0xffff0000)         ? (16 +_LOG2C((s) >>16)): (_LOG2C(s)))
 #define _LOG2C(s) (((s) &0xff00)             ? (8  +_LOG2D((s) >>8)) : (_LOG2D(s)))
 #define _LOG2D(s) (((s) &0xf0)               ? (4  +_LOG2E((s) >>4)) : (_LOG2E(s)))
 #define _LOG2E(s) (((s) &0xc)                ? (2  +_LOG2F((s) >>2)) : (_LOG2F(s)))
 #define _LOG2F(s) (((s) &0x2)                ? (1)                  : (0))
 
-#define LOG2_UINT64 __LOG2A
+#define LOG2_UINT64 _LOG2A
 #define LOG2_UINT32 _LOG2B
 #define LOG2_UINT16 _LOG2C
 #define LOG2_UINT8  _LOG2D
