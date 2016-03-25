986881f7df9c37a290beb4716a6e2de292756cf4
Lucjan Bryndza <lbryndza.p@boff.pl>
2016-03-25 20:57:37 +0100

    FIX RCC master header. Initial version for F3 MCUs

---
+++ libstm32/inc/stm32f37x/stm32rcc.h
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  stm32rcc.h
+ *
+ *    Description:  STM32 
+ *
+ *        Version:  1.0
+ *        Created:  25.03.2016 20:47:00
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Lucjan Bryndza (LB), lucck(at)boff(dot)pl
+ *
+ * =====================================================================================
+ */
+
+#pragma once
+
--- libstm32/inc/stm32rcc.h
+++ libstm32/inc/stm32rcc.h
@@ -9,10 +9,10 @@
 
 #if defined(STM32MCU_MAJOR_TYPE_F1)
 #include "stm32f1xx/stm32rcc.h"
-#elif defined(STM32MCU_MAJOR_TYPE_F4)
+#elif defined(STM32MCU_MAJOR_TYPE_F4)||defined(STM32MCU_MAJOR_TYPE_F2)
 #include "stm32f4x/stm32rcc.h"
-#elif defined(STM32MCU_MAJOR_TYPE_F2)
-#include "stm32f2x/stm32rcc.h"
+#elif defined(STM32MCU_MAJOR_TYPE_F37)||defined(STM32MCU_MAJOR_TYPE_F3)
+#include "stm32f37x/stm32rcc.h"
 #else
 #error "Unknown device type"
 #endif
