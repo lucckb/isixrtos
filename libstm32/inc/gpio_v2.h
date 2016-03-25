bceb32dd3796043f35f9cd54a223e840546a281b
Lucjan Bryndza <lbryndza.p@boff.pl>
2016-03-25 20:02:30 +0100

    Common STM32gpio for all platforms

--- libstm32/inc/gpio_v2.h
+++ libstm32/inc/gpio_v2.h
@@ -5,20 +5,22 @@
  *      Author: lucck
  */
 
-#ifndef GPIO_F4X_H_
-#define GPIO_F4X_H_
+#pragma once
 
 #include <stdint.h>
 #include <stdbool.h>
 #include "stm32lib.h"
+//Legacy alternate functions
+#if defined(STM32MCU_MAJOR_TYPE_F2) || defined(STM32MCU_MAJOR_TYPE_F4)
+#include <stm32f4x/stm32f4x_gpio.h>
+#endif
 
 #ifdef __cplusplus
 namespace stm32 {
 #endif
 
 /** GPIO bits macros */
-enum e_gpio_mode
-{
+enum e_gpio_mode {
 	//! GPIO mode input
 	GPIO_MODE_INPUT 	= 0,
 	//! GPIO mode output
@@ -31,34 +33,108 @@ enum e_gpio_mode
 
 
 /*** GPIO type ***/
-enum e_gpio_otype
-{
+enum e_gpio_otype {
 	//! Output type push pull
 	GPIO_OTYPE_PP = 0,
 	//! Output type open drain
 	GPIO_OTYPE_OD = 1
 };
 
-enum e_gpio_ospeed
-{
-	//! GPIO port speed 2MHz
-	GPIO_SPEED_2MHZ = 0,
-	//! GPIO port speed 25MHz
-	GPIO_SPEED_25MHZ = 1,
-	//! GPIO port speed 50MHz
-	GPIO_SPEED_50MHZ = 2,
-	//! GPIO port speed 100MHz
-	GPIO_SPEED_100MHZ = 3
+
+/** New recommended speed enumeration */
+enum e_lvl_gpio_ospeed {
+	GPIO_SPEED_LEVEL_0,
+	GPIO_SPEED_LEVEL_1,
+	GPIO_SPEED_LEVEL_2,
+	GPIO_SPEED_LEVEL_3,
 };
 
-enum e_gpio_pullup
-{
+/** Old GPIO speed enumeration */
+#if defined(STM32MCU_MAJOR_TYPE_F2) || defined(STM32MCU_MAJOR_TYPE_F4)
+enum e_gpio_ospeed {
+	//! GPIO port speed 2MHz
+	GPIO_SPEED_LOW = 	GPIO_SPEED_LEVEL_0,
+	//! GPIO port speed 25MHz
+	GPIO_SPEED_MED = 	GPIO_SPEED_LEVEL_1,
+	//! GPIO port speed 50MHz
+	GPIO_SPEED_FAST = 	GPIO_SPEED_LEVEL_2,
+	//! GPIO port speed 100MHz
+	GPIO_SPEED_HI = 	GPIO_SPEED_LEVEL_3
+};
+#elif defined(STM32MCU_MAJOR_TYPE_F37) || defined(STM32MCU_MAJOR_TYPE_F3) 
+enum e_gpio_ospeed {
+	//! GPIO port speed 2MHz
+	GPIO_SPEED_LOW = 	GPIO_SPEED_LEVEL_2,
+	//! GPIO port speed 10M
+	GPIO_SPEED_MED = 	GPIO_SPEED_LEVEL_1,
+	//! GPIO port speed 10M
+	GPIO_SPEED_FAST = 	GPIO_SPEED_LEVEL_1,
+	//! GPIO port speed 50M
+	GPIO_SPEED_HI = 	GPIO_SPEED_LEVEL_3
+};
+#endif
+
+
+enum e_gpio_pullup {
 	//! No pullup
 	GPIO_PUPD_NONE 		= 0,
 	//!Pull up config
 	GPIO_PUPD_PULLUP 	= 1,
 	//! Pull down config
 	GPIO_PUPD_PULLDOWN 	= 2
+};
+
+enum gpio_port_source {
+	GPIO_PortSourceGPIOA   =    0x00,
+	GPIO_PortSourceGPIOB   =    0x01,
+	GPIO_PortSourceGPIOC   =    0x02,
+	GPIO_PortSourceGPIOD   =    0x03,
+	GPIO_PortSourceGPIOE   =    0x04,
+	GPIO_PortSourceGPIOF   =    0x05,
+	GPIO_PortSourceGPIOG   =    0x06,
+	GPIO_PortSourceGPIOH   =    0x07,
+	GPIO_PortSourceGPIOI   =    0x08
+};
+
+enum gpio_pin_source {
+	GPIO_PinSource0     =       0x00,
+	GPIO_PinSource1     =       0x01,
+	GPIO_PinSource2     =       0x02,
+	GPIO_PinSource3     =       0x03,
+	GPIO_PinSource4     =       0x04,
+	GPIO_PinSource5     =       0x05,
+	GPIO_PinSource6     =       0x06,
+	GPIO_PinSource7     =       0x07,
+	GPIO_PinSource8     =       0x08,
+	GPIO_PinSource9     =       0x09,
+	GPIO_PinSource10    =       0x0A,
+	GPIO_PinSource11    =       0x0B,
+	GPIO_PinSource12    =       0x0C,
+	GPIO_PinSource13    =       0x0D,
+	GPIO_PinSource14    =       0x0E,
+	GPIO_PinSource15    =       0x0F
+};
+
+
+/** GPIO alternate function */
+enum gpio_af
+{
+	GPIO_AF_0,
+	GPIO_AF_1,
+	GPIO_AF_2,
+	GPIO_AF_3,
+	GPIO_AF_4,
+	GPIO_AF_5,
+	GPIO_AF_6,
+	GPIO_AF_7,
+	GPIO_AF_8,
+	GPIO_AF_9,
+	GPIO_AF_10,
+	GPIO_AF_11,
+	GPIO_AF_12,
+	GPIO_AF_13,
+	GPIO_AF_14,
+	GPIO_AF_15
 };
 
 //! Set GPIO bit macro
@@ -137,123 +213,6 @@ static inline void gpio_config(GPIO_TypeDef* port, uint8_t bit , uint16_t mode, 
 	port->PUPDR &= ~(C_pupd_mask << (2*bit));
 	port->PUPDR |= ((uint32_t)pullup & C_mode_mask) << (2*bit);
 }
-
-enum gpio_port_source
-{
-	GPIO_PortSourceGPIOA   =    0x00,
-	GPIO_PortSourceGPIOB   =    0x01,
-	GPIO_PortSourceGPIOC   =    0x02,
-	GPIO_PortSourceGPIOD   =    0x03,
-	GPIO_PortSourceGPIOE   =    0x04,
-	GPIO_PortSourceGPIOF   =    0x05,
-	GPIO_PortSourceGPIOG   =    0x06,
-	GPIO_PortSourceGPIOH   =    0x07,
-	GPIO_PortSourceGPIOI   =    0x08
-};
-enum gpio_pin_source
-{
-	GPIO_PinSource0     =       0x00,
-	GPIO_PinSource1     =       0x01,
-	GPIO_PinSource2     =       0x02,
-	GPIO_PinSource3     =       0x03,
-	GPIO_PinSource4     =       0x04,
-	GPIO_PinSource5     =       0x05,
-	GPIO_PinSource6     =       0x06,
-	GPIO_PinSource7     =       0x07,
-	GPIO_PinSource8     =       0x08,
-	GPIO_PinSource9     =       0x09,
-	GPIO_PinSource10    =       0x0A,
-	GPIO_PinSource11    =       0x0B,
-	GPIO_PinSource12    =       0x0C,
-	GPIO_PinSource13    =       0x0D,
-	GPIO_PinSource14    =       0x0E,
-	GPIO_PinSource15    =       0x0F
-};
-//GPIO alternate functions
-enum { GPIO_AF_RTC_50Hz = 0   };  /* RTC_50Hz Alternate Function mapping */
-enum { GPIO_AF_MCO      = 0   };  /* MCO (MCO1 and MCO2) Alternate Function mapping */
-enum { GPIO_AF_TAMPER   = 0   };  /* TAMPER (TAMPER_1 and TAMPER_2) Alternate Function mapping */
-enum { GPIO_AF_SWJ      = 0   };  /* SWJ (SWD and JTAG) Alternate Function mapping */
-enum { GPIO_AF_TRACE    = 0   };  /* TRACE Alternate Function mapping */
-/**
-  * @brief   AF 1 selection
-  */
-enum { GPIO_AF_TIM1 =  0x01   };  /* TIM1 Alternate Function mapping */
-enum { GPIO_AF_TIM2 =  0x01   };  /* TIM2 Alternate Function mapping */
-/**
-  * @brief   AF 2 selection
-  */
-enum { GPIO_AF_TIM3     = 0x02  };  /* TIM3 Alternate Function mapping */
-enum { GPIO_AF_TIM4     = 0x02  };  /* TIM4 Alternate Function mapping */
-enum { GPIO_AF_TIM5     = 0x02  };  /* TIM5 Alternate Function mapping */
-/**
-  * @brief   AF 3 selection
-  */
-enum { GPIO_AF_TIM8      =  0x03  };  /* TIM8 Alternate Function mapping */
-enum { GPIO_AF_TIM9      =  0x03  };  /* TIM9 Alternate Function mapping */
-enum { GPIO_AF_TIM10     =  0x03  };  /* TIM10 Alternate Function mapping */
-enum { GPIO_AF_TIM11     =  0x03  };  /* TIM11 Alternate Function mapping */
-/**
-  * @brief   AF 4 selection
-  */
-enum {  GPIO_AF_I2C1     =  0x04  }; /* I2C1 Alternate Function mapping */
-enum {  GPIO_AF_I2C2     =  0x04  }; /* I2C2 Alternate Function mapping */
-enum {  GPIO_AF_I2C3     =  0x04  }; /* I2C3 Alternate Function mapping */
-/**
-  * @brief   AF 5 selection
-  */
-enum { GPIO_AF_SPI1    = 0x05  };  /* SPI1 Alternate Function mapping */
-enum { GPIO_AF_SPI2    = 0x05  };  /* SPI2/I2S2 Alternate Function mapping */
-/**
-  * @brief   AF 6 selection
-  */
-enum { GPIO_AF_SPI3  = 6  };     /* SPI3/I2S3 Alternate Function mapping */
-/**
-  * @brief   AF 7 selection
-  */
-enum { GPIO_AF_USART1       = 0x07  };  /* USART1 Alternate Function mapping */
-enum { GPIO_AF_USART2       = 0x07  };  /* USART2 Alternate Function mapping */
-enum { GPIO_AF_USART3       = 0x07  };  /* USART3 Alternate Function mapping */
-enum { GPIO_AF_I2S3ext      = 0x07  };  /* I2S3ext Alternate Function mapping */
-/**
-  * @brief   AF 8 selection
-  */
-enum { GPIO_AF_UART4     = 0x08 };  /* UART4 Alternate Function mapping */
-enum { GPIO_AF_UART5     = 0x08 };  /* UART5 Alternate Function mapping */
-enum { GPIO_AF_USART6    = 0x08 };  /* USART6 Alternate Function mapping */
-/**
-  * @brief   AF 9 selection
-  */
-enum { GPIO_AF_CAN1    =   0x09  };  /* CAN1 Alternate Function mapping */
-enum { GPIO_AF_CAN2    =   0x09  };  /* CAN2 Alternate Function mapping */
-enum { GPIO_AF_TIM12   =   0x09  };  /* TIM12 Alternate Function mapping */
-enum { GPIO_AF_TIM13   =   0x09  };  /* TIM13 Alternate Function mapping */
-enum { GPIO_AF_TIM14   =   0x09  };  /* TIM14 Alternate Function mapping */
-/**
-  * @brief   AF 10 selection
-  */
-enum {  GPIO_AF_OTG_FS    =  0xA };  /* OTG_FS Alternate Function mapping */
-enum {  GPIO_AF_OTG_HS    =  0xA };  /* OTG_HS Alternate Function mapping */
-enum {  GPIO_AF_OTG2_HS    =  0xA };  /* OTG_HS Alternate Function mapping */
-/**
-  * @brief   AF 11 selection
-  */
-enum { GPIO_AF_ETH       = 0x0B  }; /* ETHERNET Alternate Function mapping */
-/**
-  * @brief   AF 12 selection
-  */
-enum { GPIO_AF_FSMC         =  0xC  };  /* FSMC Alternate Function mapping */
-enum { GPIO_AF_OTG_HS_FS    =  0xC  };  /* OTG HS configured in FS, Alternate Function mapping */
-enum { GPIO_AF_SDIO         =  0xC  };  /* SDIO Alternate Function mapping */
-/**
-  * @brief   AF 13 selection
-  */
-enum { GPIO_AF_DCMI     =  0x0D  };  /* DCMI Alternate Function mapping */
-/**
-  * @brief   AF 15 selection
-  */
-enum {  GPIO_AF_EVENTOUT   =    0x0F };  /* EVENTOUT Alternate Function mapping */
-
 
 
 
@@ -334,44 +293,6 @@ static inline void gpio_pin_lock_config(GPIO_TypeDef* port, uint16_t bit )
   *            @arg GPIO_AF_RTC_50Hz: Connect RTC_50Hz pin to AF0 (default after reset)
   *            @arg GPIO_AF_MCO: Connect MCO pin (MCO1 and MCO2) to AF0 (default after reset)
   *            @arg GPIO_AF_TAMPER: Connect TAMPER pins (TAMPER_1 and TAMPER_2) to AF0 (default after reset)
-  *            @arg GPIO_AF_SWJ: Connect SWJ pins (SWD and JTAG)to AF0 (default after reset)
-  *            @arg GPIO_AF_TRACE: Connect TRACE pins to AF0 (default after reset)
-  *            @arg GPIO_AF_TIM1: Connect TIM1 pins to AF1
-  *            @arg GPIO_AF_TIM2: Connect TIM2 pins to AF1
-  *            @arg GPIO_AF_TIM3: Connect TIM3 pins to AF2
-  *            @arg GPIO_AF_TIM4: Connect TIM4 pins to AF2
-  *            @arg GPIO_AF_TIM5: Connect TIM5 pins to AF2
-  *            @arg GPIO_AF_TIM8: Connect TIM8 pins to AF3
-  *            @arg GPIO_AF_TIM9: Connect TIM9 pins to AF3
-  *            @arg GPIO_AF_TIM10: Connect TIM10 pins to AF3
-  *            @arg GPIO_AF_TIM11: Connect TIM11 pins to AF3
-  *            @arg GPIO_AF_I2C1: Connect I2C1 pins to AF4
-  *            @arg GPIO_AF_I2C2: Connect I2C2 pins to AF4
-  *            @arg GPIO_AF_I2C3: Connect I2C3 pins to AF4
-  *            @arg GPIO_AF_SPI1: Connect SPI1 pins to AF5
-  *            @arg GPIO_AF_SPI2: Connect SPI2/I2S2 pins to AF5
-  *            @arg GPIO_AF_SPI3: Connect SPI3/I2S3 pins to AF6
-  *            @arg GPIO_AF_I2S3ext: Connect I2S3ext pins to AF7
-  *            @arg GPIO_AF_USART1: Connect USART1 pins to AF7
-  *            @arg GPIO_AF_USART2: Connect USART2 pins to AF7
-  *            @arg GPIO_AF_USART3: Connect USART3 pins to AF7
-  *            @arg GPIO_AF_UART4: Connect UART4 pins to AF8
-  *            @arg GPIO_AF_UART5: Connect UART5 pins to AF8
-  *            @arg GPIO_AF_USART6: Connect USART6 pins to AF8
-  *            @arg GPIO_AF_CAN1: Connect CAN1 pins to AF9
-  *            @arg GPIO_AF_CAN2: Connect CAN2 pins to AF9
-  *            @arg GPIO_AF_TIM12: Connect TIM12 pins to AF9
-  *            @arg GPIO_AF_TIM13: Connect TIM13 pins to AF9
-  *            @arg GPIO_AF_TIM14: Connect TIM14 pins to AF9
-  *            @arg GPIO_AF_OTG_FS: Connect OTG_FS pins to AF10
-  *            @arg GPIO_AF_OTG_HS: Connect OTG_HS pins to AF10
-  *            @arg GPIO_AF_ETH: Connect ETHERNET pins to AF11
-  *            @arg GPIO_AF_FSMC: Connect FSMC pins to AF12
-  *            @arg GPIO_AF_OTG_HS_FS: Connect OTG HS (configured in FS) pins to AF12
-  *            @arg GPIO_AF_SDIO: Connect SDIO pins to AF12
-  *            @arg GPIO_AF_DCMI: Connect DCMI pins to AF13
-  *            @arg GPIO_AF_EVENTOUT: Connect EVENTOUT pins to AF15
-  * @retval None
   */
 /** Setup alternate function for the pin
  * @param[in] GPIOx Gpio port
@@ -429,4 +350,3 @@ static inline void gpio_clock_enable( GPIO_TypeDef* port, bool enable )
 #ifdef __cplusplus
 }
 #endif
-#endif /* GPIO_F1X_H_ */
