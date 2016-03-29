1c9c8fc9565091338e1d2d181c9acefc37ae8e96
Lucjan Bryndza <lbryndza.p@boff.pl>
2016-03-29 14:27:20 +0200

    Usart simple compilation fix

--- libstm32/src/usart_simple.c
+++ libstm32/src/usart_simple.c
@@ -1,16 +1,17 @@
-/* ---------------------------------------------------------------------------- */
+
 /*
  * usart_simple.c
  *
  *  Created on: 2009-10-13
  *      Author: lucck
  */
-/* ---------------------------------------------------------------------------- */
+
 #include <stm32lib.h>
+#include <stm32usart.h>
 #include "stm32system.h"
 #include "usart_simple.h"
 
-/* ---------------------------------------------------------------------------- */
+
 
 #define USART1_TX_BIT 9
 #define USART1_RX_BIT 10
@@ -20,28 +21,11 @@
 #define USART2_ALT_TX_BIT 5
 #define USART2_ALT_RX_BIT 6
 #define USART2_RX_BIT 3
-#define CR1_UE_SET 0x2000
-#define CCR_ENABLE_SET 0x00000001
-#define USART1_DR_BASE  0x40013804
-#define USART_M_BIT (1<<12)
-#define USART_PCE_BIT (1<<10)
-#define USART_PS_BIT (1<<9)
-#define USART_TXEIE (1<<7)
-#define USART_RXNEIE (1<<5)
-#define USART_RXNE (1<<5)
-#define USART_TXE (1<<7)
-#define USART_ORE (1<<3)
-#define USART_TC (1<<6)
-#define USART_FE (1<<1)
-#define USART_PE (1<<0)
-#define USART_ORE (1<<3)
 
-
-/* ---------------------------------------------------------------------------- */
 
 static USART_TypeDef *usart;
 
-/* ---------------------------------------------------------------------------- */
+
 int usartsimple_init(USART_TypeDef *usart_, unsigned baudrate, bool alternate,
 		unsigned long pclk1_hz, unsigned long pclk2_hz)
 {
@@ -105,60 +89,45 @@ int usartsimple_init(USART_TypeDef *usart_, unsigned baudrate, bool alternate,
 		return USARTSIMPLE_INIT_FAIL;
 	}
 	usart = usart_;
-	usart->CR1 &= ~(USART_M_BIT | USART_PCE_BIT);
-    //Enable UART
-    usart->CR1 = CR1_UE_SET;
-    unsigned long pclk_hz;
-    if(usart==USART1) pclk_hz = pclk2_hz;
-    else pclk_hz = pclk1_hz;
-    //Calculate baud rate
-    uint32_t int_part = ((0x19 * pclk_hz) / (0x04 * baudrate));
-    uint32_t tmp = (int_part / 0x64) << 0x04;
-    uint32_t fract_part = int_part - (0x64 * (tmp >> 0x04));
-    tmp |= ((((fract_part * 0x10) + 0x32) / 0x64)) & ((u8)0x0F);
-    usart->BRR = tmp;
-    //Parity none
-    usart->CR1 &= ~(USART_M_BIT | USART_PCE_BIT);
-    //One stop bit
-    usart->CR2 = USART_StopBits_1;
-    //Enable receiver and transmitter and anable related interrupts
-    usart->CR1 |=  USART_Mode_Tx | USART_Mode_Rx;
+	usart_init( usart, baudrate, USART_WordLength_8b, USART_StopBits_1,
+			    USART_Parity_No, USART_Mode_Rx|USART_Mode_Tx, 
+				USART_HardwareFlowControl_None, pclk1_hz, pclk2_hz );
     return USARTSIMPLE_INIT_OK;
 }
 
-/* ---------------------------------------------------------------------------- */
+
 //USART putchar
 int usartsimple_putc(int ch, void* p)
 {
 	(void)p;
 	if(!usart) return USARTSIMPLE_NOT_INIT;
-	while((usart->SR & USART_TXE)==0);
-    usart->DR = ch & 0xff;
+	while( !usart_get_flag_status(usart, USART_FLAG_TXE) );
+    usart_send_data( usart, ch );
     return ch;
 }
-/* ---------------------------------------------------------------------------- */
+
 //Usart get char
 int usartsimple_getc(void)
 {
 	if(!usart) return USARTSIMPLE_NOT_INIT;
-	if(usart->SR & USART_RXNE)
-		return usart->DR & 0xff;
+	if( usart_get_flag_status( usart, USART_FLAG_RXNE ) )
+		return usart_receive_data( usart );
 	else
 		return USARTSIMPLE_EOF;
 }
-/* ---------------------------------------------------------------------------- */
+
 //Check if char is available
 int usartsimple_isc(void)
 {
 	if(!usart) return USARTSIMPLE_NOT_INIT;
-	return usart->SR & USART_RXNE;
+	return usart_get_flag_status( usart, USART_FLAG_RXNE );
 }
-/* ---------------------------------------------------------------------------- */
+
 void usartsimple_puts(const char * str)
 {
 	while(*str)
 		usartsimple_putc(*str++,0);
 }
-/* ---------------------------------------------------------------------------- */
+
 
 
