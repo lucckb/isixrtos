70bbec30758324427d6ee1b74527003d696003de
Lucjan Bryndza <lbryndza.p@boff.pl>
2016-03-30 20:08:17 +0200

    Inital emeter library

---
+++ libenergymeter/include/emeter/types.hpp
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  types.hpp
+ *
+ *    Description:  Open EFM types
+ *
+ *        Version:  1.0
+ *        Created:  30.03.2016 19:54:13
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Lucjan Bryndza (LB), lucck(at)boff(dot)pl
+ *   Organization:  BoFF
+ *
+ * =====================================================================================
+ */
+
+#pragma once
+
+namespace emeter {
+namespace detail {
+	//! Type used for energy calculation
+	using energy_t = double;
+	//! Type used for voltage calculation
+	using voltage_t = float;
+	//! Type used for current calculation
+	using current_t = float;
+	//! Type used for sampling 
+	using sample_t =  unsigned short;
+}
+}
