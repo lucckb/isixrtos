242069780273c8b8cab57aadbebfac3e2ed3e5e7
Lucjan Bryndza <lbryndza.p@boff.pl>
2016-03-31 22:17:10 +0200

    Initial config for PC test

---
+++ tests/libenergymeter/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  Main emtest
+ *
+ *        Version:  1.0
+ *        Created:  31.03.2016 21:37:44
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Lucjan Bryndza (LB), lucck(at)boff(dot)pl
+ *   Organization:  BoFF
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello " << std::endl;
+}
+
+
