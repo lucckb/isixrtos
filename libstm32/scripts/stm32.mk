b6b3dffc74c0b596f82b5ef6cf8813c8106dd190
Lucjan Bryndza <lbryndza.p@boff.pl>
2016-04-22 21:38:03 +0200

    C++14 is default C++ dialect now Added sized deallocators

--- libstm32/scripts/stm32.mk
+++ libstm32/scripts/stm32.mk
@@ -97,7 +97,7 @@ else
 COMMON_FLAGS += -DSTM32MCU_MAJOR_TYPE_F1
 endif
 endif
-CXXFLAGS += -std=gnu++11
+CXXFLAGS += -std=gnu++14
 ASFLAGS += -Wa,-mapcs-32 -mcpu=$(MCU) -mthumb $(COMMON_FLAGS)
 LDFLAGS +=  -L$(SCRIPTS_DIR) -nostdlib -nostartfiles -T$(LSCRIPT) -Wl,-Map=$(TARGET).map,--cref -mthumb
 CPFLAGS =  -S
