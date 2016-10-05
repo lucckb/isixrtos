9c9e8db10364f87b1f68a7ab909da63592f54f40
Lucjan Bryndza <lbryndza.p@boff.pl>
2016-10-05 22:23:50 +0200

    Pefcent calculation

--- tests/libenergymeter/fft_tests_common.hpp
+++ tests/libenergymeter/fft_tests_common.hpp
@@ -32,29 +32,36 @@ namespace cfg {
 		//! Maximum Nth points used in tests
 		constexpr auto fft_max = 16384;
 
+		constexpr double proc( double perc, double max ) {
+			return (max * perc) / 100.0;
+		}
 		template< typename T > struct ans {
 		};
 		//! Type IFFFT
 		template<> struct ans<ifft_t> {
+			static constexpr auto tol = 0.1;
+			static constexpr double max = std::numeric_limits<ifft_t>::max();
 			//Maximum difference for shifted_impulse_response
-			static constexpr auto max_shifted_resp = 3;
+			static constexpr auto max_shifted_resp = proc(tol,max);
 			//Tone test fft real symetry error check
-			static constexpr auto symetry_err = 32;
+			static constexpr auto symetry_err = proc(tol,max);
 			//! Signal to noise ratio tolerance
 			static constexpr auto snr_err = 42;
 			//! Compare two ffts err
-			static constexpr auto fft_res_cmp_err = 9;
+			static constexpr auto fft_res_cmp_err = proc(tol,max);
 		};
 		//! Type RFFT
 		template<> struct ans<rfft_t> {
+			static constexpr auto tol = 0.001;
+			static constexpr double max = 1.0;
 			//Maximum difference for shifted_impulse_response
-			static constexpr auto max_shifted_resp = 1E-12;
+			static constexpr auto max_shifted_resp = proc(tol,max);
 			//Tone test fft real symetry error check
-			static constexpr auto symetry_err = 0.01;
+			static constexpr auto symetry_err = proc(tol,max);
 			//! Signal to noise ratio tolerance
 			static constexpr auto snr_err = 200.0;
 			//! Compare two ffts err
-			static constexpr auto fft_res_cmp_err = 0.0001;
+			static constexpr auto fft_res_cmp_err = proc(tol,max);
 		};
 		//Float precision floating point
 		template<> struct ans<ffft_t> {
@@ -69,14 +76,16 @@ namespace cfg {
 		};
 		//! Type int32 integer aritmetics
 		template<> struct ans<lfft_t> {
+			static constexpr auto tol = 0.001;
+			static constexpr double max = std::numeric_limits<lfft_t>::max();
 			//Maximum difference for shifted_impulse_response
-			static constexpr auto max_shifted_resp = 3;
+			static constexpr auto max_shifted_resp = proc(tol,max);
 			//Tone test fft real symetry error check
-			static constexpr auto symetry_err = 32;
+			static constexpr auto symetry_err = proc(tol,max);
 			//! Signal to noise ratio tolerance
 			static constexpr auto snr_err = 130;
 			//! Compare two ffts err
-			static constexpr auto fft_res_cmp_err = 8;
+			static constexpr auto fft_res_cmp_err = proc(tol,max);
 		};
 }
 		template<typename T> T get_rand()
