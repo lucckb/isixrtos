7780db3b3a0a38016e09ae6d902b734934a117af
Lucjan Bryndza <lbryndza.p@boff.pl>
2016-10-08 21:32:45 +0200

    Integer FFT fixed. Sinus linear interpolation implemented for better accuracy

--- libfoundation/include/foundation/dsp/fft.hpp
+++ libfoundation/include/foundation/dsp/fft.hpp
@@ -7,156 +7,14 @@
 #include <cstring>
 #include <complex>
 #include "array_sinus.hpp"
+#include "_fft_helper_internal.hpp"
 
 
 namespace dsp {
 namespace refft {
 
-namespace _internal
-{
-		template< typename T > struct limits {
-		};
-
-		template<> struct limits <int16_t> {
-			using acc_t = int32_t;
-		};
-
-		template<> struct limits <int32_t> {
-			using acc_t = int64_t;
-		};
-
-		//Generate SIN cos array in C99 lang
-		template<unsigned... Is> struct seq{};
-		template<unsigned N, unsigned... Is>
-		struct gen_seq : gen_seq<N-1, N-1, Is...>{};
-		template<unsigned... Is>
-		struct gen_seq<0, Is...> : seq<Is...>{};
-
-		template <typename T, size_t S=128>
-	    class fft_sin_cos
-	    {
-	    public:
-			constexpr std::complex<T> operator()( const size_t m ) const
-			{
-		        return sine_array[m];
-		    }
-			std::complex<T> sine_array[S];
-	    };
-
-		template <typename T>
-		constexpr std::complex<T> get_mfft_tab(const size_t idx)
-		{
-		    return
-		    {
-				T(  std::cos( (4.0 * std::atan(1.0)) / double(1<<idx) )  ),
-			    T(  -std::sin( (4.0 * std::atan(1.0)) / double(1<<idx) ) )
-			};
-		}
-		template<typename T, size_t S, unsigned... Is>
-		constexpr fft_sin_cos<T,S> create_fft_sin_cos( seq<Is...>)
-		{
-			return {{ get_mfft_tab<T>(Is)... }};
-		}
-
-		template <typename T, size_t S>
-		constexpr fft_sin_cos<T,S> create_fft_sin_cos(  )
-		{
-		    return create_fft_sin_cos<T,S>(gen_seq<S>{});
-		}
-		template<typename T, size_t S=17>
-		inline std::complex<T>  fft_sincos_get( const size_t idx )
-		{
-			static constexpr fft_sin_cos<T,S> sin_tab = create_fft_sin_cos<T,S>( );
-			return ( sin_tab(idx) );
-		}
-		//Bit reverseW
-		#ifndef __arm__
-		//Generic bitrev version
-		inline unsigned bitrev( unsigned v, int m )
-		{
-			// swap odd and even bits
-			v = ((v >> 1) & 0x55555555) | ((v & 0x55555555) << 1);
-			// swap consecutive pairs
-			v = ((v >> 2) & 0x33333333) | ((v & 0x33333333) << 2);
-			// swap nibbles ... 
-			v = ((v >> 4) & 0x0F0F0F0F) | ((v & 0x0F0F0F0F) << 4);
-			// swap bytes
-			v = ((v >> 8) & 0x00FF00FF) | ((v & 0x00FF00FF) << 8);
-			// swap 2-byte long pairs
-			v = ( v >> 16             ) | ( v               << 16);
-			return v >> (32 - m);
-		}
-		#else
-		inline unsigned bitrev( unsigned value, int m )
-		{
-			unsigned result;
-			asm ("rbit %0, %1" : "=r" (result) : "r" (value) );
-			return result >> ( 32 - m );
-		}
-		#endif
-#if 1
-		//Constant class definition
-		template<typename T> class scaledV
-		{
-			public:
-				constexpr scaledV( double re, double im )
-					: m_v(
-							re * ( ( std::is_floating_point<T>::value)?(1.0):(std::numeric_limits<T>::max())),
-							im * ( ( std::is_floating_point<T>::value)?(1.0):(std::numeric_limits<T>::max()))
-						 )
-					{
-					}
-				constexpr operator std::complex<T>()
-				{
-					return m_v;
-				}
-			private:
-				std::complex<T> m_v;
-		};
-#endif
-		//Multiply integer
-		template<typename T,typename A=typename limits<T>::acc_t >
-		typename std::enable_if<std::is_integral<T>::value, T>::type
-		inline impy( T x, T y )
-		{
-			static constexpr T scale = std::numeric_limits<T>::max();
-			return (A(x) * A(y))/scale;
-		}
-		//Multiply floating
-		template<typename T, typename A>
-		typename std::enable_if<std::is_floating_point<T>::value, T>::type
-		inline impy( T x, T y )
-		{
-			return (x * y);
-		}
-		//Scale added for floating point arithmetic
-		template<class T>
-		typename std::enable_if<std::is_floating_point<T>::value, std::complex<T>>::type
-		    inline fft_scale(std::complex<T> t )
-		{
-		    return t;
-		}
-		//Scale for integer value
-		template<class T>
-		typename std::enable_if<std::is_integral<T>::value, std::complex<T>>::type
-			inline fft_scale(std::complex<T> v )
-		{
-			return std::complex<T> (v.real() >> 1, v.imag() >> 1);
-		}
-		//Scale added for floating point arithmetic
-		template<class T>
-		typename std::enable_if<std::is_floating_point<T>::value, T>::type
-			inline fft_scale( T t )
-		{
-			return t;
-		}
-		template<class T>
-		typename std::enable_if<std::is_integral<T>::value, T>::type
-			inline fft_scale( T v )
-		{
-			return v>>1;
-		}
-
+namespace _internal {
+namespace {
 		//FFT STAGE FLOAT VERSION
 		template<typename T>
 		typename std::enable_if<std::is_floating_point<T>::value, std::complex<T>*>::type
@@ -167,10 +25,10 @@ namespace _internal
 			const int n = 1 << m;
 			std::complex<T> u( 1.0, 0.0 );
 			const std::complex<T> s (fft_sincos_get<T>(l-1));
-			//Kazde widmo
+			//Each spectrum
 			for(int j=0;j<le2;j++)
 			{
-				//Motylek
+				//Butterfly
 				for(int i=j;i<n;i+=le)
 				{
 					const int ip = i + le2;
@@ -188,27 +46,7 @@ namespace _internal
 			}
 			return x;
 		}
-		//Internal SINUS integer version
-		template <typename T>
-		inline std::complex<T> fft_sin_cos_int( int le2, int i )
-		{
-			constexpr auto SINSIZE = 256;
-			constexpr auto CPI = dsp::integer::trig::sin_arg_max<T,SINSIZE>();
-#if 1
-			const auto angle = (CPI/2*i)/le2;
 
-			return {
-				  T((dsp::integer::trig::sin<T,SINSIZE>(CPI/4+angle) + dsp::integer::trig::sin<T,SINSIZE>(CPI/4+angle))/2),
-			      T(-(dsp::integer::trig::sin<T,SINSIZE>(angle) + dsp::integer::trig::sin<T,SINSIZE>(angle))/2)
-			};
-#else
-			const int ii = CPI/2*i;
-			return {
-				   T(dsp::integer::trig::cos<T,SINSIZE>(ii,le2) ),
-				   T(-dsp::integer::trig::sin<T,SINSIZE>(ii,le2) )
-			};
-#endif
-		}
 		//FFT stage INTEGER VERSION
 		template<typename T>
 		typename std::enable_if<std::is_integral<T>::value, std::complex<T>*>::type
@@ -220,16 +58,16 @@ namespace _internal
 			//Kazde widmo
 			for(int j=0;j<le2;j++)
 			{
-				const std::complex<T> s = scaledV<T>( cosf( (float(M_PI)*j)/float(le2) ), -sinf( (float(M_PI)*j)/float(le2)) );
-				//const std::complex<T> s = scaledV<T>( cos( (double(M_PI)*j)/double(le2) ), -sin( (double(M_PI)*j)/double(le2)) );
-				//const std::complex<T> s( fft_sin_cos_int<T>(le2, j) );
-				//Motylek
+				const std::complex<T> s( fft_sin_cos_int<T>(le2, j) );
+				//Butterfly
 				for(int i=j;i<n;i+=le)
 				{
 					const int ip = i + le2;
 					const std::complex<T> t(
-						impy<T>(x[ip].real() , fft_scale(s.real())) - impy<T>( x[ip].imag() , fft_scale(s.imag()) ),
-						impy<T>(x[ip].imag() , fft_scale(s.real())) + impy<T>( x[ip].real() , fft_scale(s.imag()) )
+						impy<T>(x[ip].real() , fft_scale(s.real())) -
+							impy<T>( x[ip].imag() , fft_scale(s.imag()) ),
+						impy<T>(x[ip].imag() , fft_scale(s.real())) +
+							impy<T>( x[ip].real() , fft_scale(s.imag()) )
 					);
 					x[ip] = fft_scale(x[i]) - t;
 					x[i] =  fft_scale(x[i]) + t;
@@ -237,7 +75,7 @@ namespace _internal
 			}
 			return x;
 		}
-}	//Internal namespace end
+}}	//Internal namespace end
 
 
 //Calculate complex fft
@@ -246,7 +84,7 @@ void fft_complex( std::complex<T>* x, const std::complex<T>* const in, int m )
 {
 	using namespace _internal;
 	const int n = 1 << m;
-	//Odwracanie bitowe
+	//Bit reverse
 	if( x != in ) std::memcpy( x, in, n*sizeof(std::complex<T>) );
 	for (int s=0,i=0;s<n-1;s++)
 	{
@@ -292,9 +130,9 @@ template<typename T>
 void fft_real( std::complex<T> *xc, const T * const x, int m )
 {
 	 using namespace _internal;
-	 //Rozklad parzysto-nieparzysty w dziedzinie czasu
+	 //Odd even extraction in the time domain
 	 fft_complex<T>( xc, reinterpret_cast<const std::complex<T>* const>(x), m-1 );
-	 //Rozklad parzysto nieparzysty w dziedzinie czestotliwosci
+	 //Odd even extraction in the frequency domain
 	 for(int i=1;i<((1<<m)/4);i++)
 	 {
 	    const int im = (1<<m)/2 - i;
@@ -315,7 +153,7 @@ void fft_real( std::complex<T> *xc, const T * const x, int m )
 	 xc[(1<<m)/2].imag( 0 );
 	 xc[(1<<m)/4].imag( 0 );
 	 xc[0].imag( 0 );
-	 //Ostatni stopien fft (trzeba powtorzyc)
+	 //Last fft butterfly ( need to be recalculated again )
 	 fft_stage_3_n<T>( xc, m, m );
 }
 
