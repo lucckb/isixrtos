76faa9484f7b37a484c99116f75a98b4693534e0
Lucjan Bryndza <lbryndza.p@boff.pl>
2016-12-04 21:16:27 +0100

    Git describe update

--- libisix/isix.mk
+++ libisix/isix.mk
@@ -43,7 +43,7 @@ ifeq ($(DEBUG),y)
 ISIX_INC += -DISIX_DEBUG
 endif
 
-ISIX_VERSION := $(shell git describe --tags 2> /dev/null )
+ISIX_VERSION := $(shell cd $(ISIX_DIR) && git describe --tags 2> /dev/null )
 ifdef ISIX_VERSION
 COMMON_FLAGS += -DISIX_GIT_VERSION=\"$(ISIX_VERSION)\"
 else
