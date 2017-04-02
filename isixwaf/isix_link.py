600bda237ba650bef0305d2ee7980e0fa60bf268
Lucjan Bryndza <lbryndza.p@boff.pl>
2017-04-02 21:38:32 +0200

    Added support for bootloader shift

--- isixwaf/isix_link.py
+++ isixwaf/isix_link.py
@@ -36,10 +36,13 @@ def force_static_linking(self):
 @feature('cprogram', 'cxxprogram' )
 @before('apply_link')
 def generate_ldcript(self):
+    linker_memmap = self.bld.isix_get_link_memmap()
+    if self.env.BOOTLOADER_REMAP_SHIFT_KB:
+        _bootloader_update_linker_map( linker_memmap, self.env.BOOTLOADER_REMAP_SHIFT_KB )
     self.create_task('generate_link_for_cpu',
             self.env.CRT0_LINKER_SCRIPT,
             self.path.find_or_declare( _tmplink_file ),
-            memmap=self.bld.isix_get_link_memmap()
+            memmap=linker_memmap
     )
     #from pprint import pprint
     #pprint(vars(self))
@@ -99,5 +102,37 @@ class generate_link_for_cpu(Task.Task):
         return lines
 
 
+# Update bootloader linker map
+def _bootloader_update_linker_map( link_map, remap_kb ):
+    fl_origin = int(link_map['flash']['origin'],16)
+    fl_size =_si_size_to_int(link_map['flash']['length'])
+    remap = remap_kb * 1024
+    fl_origin = fl_origin + remap
+    fl_size = fl_size - remap
+    link_map['flash']['origin'] = '0x%X' % fl_origin
+    link_map['flash']['length'] = _int_to_si_size(fl_size)
 
 
+# Convert SI to INT
+def _si_size_to_int( sstr ):
+    sstr = sstr.strip().upper()
+    mul = 1
+    if sstr.endswith('K'):
+        mul = 1024
+        sstr = sstr[:-1]
+    elif sstr.endswith('M'):
+        mul = 1024 * 1024
+        sstr = sstr[:-1]
+    return int(sstr)*mul
+
+# Convert INT to SI
+def _int_to_si_size( siz ):
+    suffix = ''
+    div = 1
+    if siz % (1024*1024) == 0:
+        suffix = 'M'
+        div = 1024*1024
+    elif siz % 1024 == 0:
+        suffix = 'K'
+        div = 1024
+    return '%i%s' %( siz/div, suffix )
