38c72b6b3427cde502146bf42a8d15eafbad9b99
Lucjan Bryndza <lbryndza.p@boff.pl>
2017-04-14 20:22:52 +0200

    IRQ global interrupt

--- libisix/arch/arm/kernel/irq_global.c
+++ libisix/arch/arm/kernel/irq_global.c
@@ -16,20 +16,69 @@
  * =====================================================================================
  */
 
-//TODO fixthis
-//
-
 #include <isix/arch/irq_global.h>
 
-#if defined(__ARM_ARCH_7EM__) || defined(__ARM_ARCH_6M__)
-#define IRQMASK_REG_NAME_R "primask"
-#define IRQMASK_REG_NAME_W "primask"
-#define IRQMASK_I_BIT	1
-#else
-#define IRQMASK_REG_NAME_R "cpsr"
-#define IRQMASK_REG_NAME_W "cpsr_c"
-#define IRQMASK_I_BIT	PSR_I_BIT
+
+
+#if defined(__ARM_ARCH_7M__)   \
+    || defined(__ARM_ARCH_7EM__) \
+    || defined(__ARM_ARCH_6M__)
+# define THUMB_V7M_V6M
 #endif
+
+
+#if defined(__ARM_ARCH_7EM__) || defined(__ARM_ARCH_7M__)
+#	define IRQMASK_REG_NAME_R "primask"
+#	define IRQMASK_REG_NAME_W "primask"
+#	define IRQMASK_I_BIT	1
+#else
+#	define IRQMASK_REG_NAME_R "cpsr"
+#	define IRQMASK_REG_NAME_W "cpsr_c"
+#	define IRQMASK_I_BIT	0x00000080
+#endif
+
+
+#ifdef THUMB_V7M_V6M
+
+/** Global IRQ enable */
+void isix_irq_enable()
+{
+	asm volatile(
+		"	cpsie i			@ arch_local_irq_enable"
+		:
+		:
+		: "memory", "cc");
+}
+
+
+/** Global IRQ disable */
+void isix_irq_disable()
+{
+	asm volatile(
+		"	cpsid i			@ arch_local_irq_disable"
+		:
+		:
+		: "memory", "cc");
+}
+
+
+/** Disable global interrupt and save IRQ status
+ * @return Current interrupt mask
+ */
+unsigned isix_irq_save(void)
+{
+	unsigned long flags;
+
+	asm volatile(
+		"	mrs	%0, " IRQMASK_REG_NAME_R "	@ arch_local_irq_save\n"
+		"	cpsid	i"
+		: "=r" (flags) : : "memory", "cc");
+	return flags;
+}
+
+
+#else /* THUMB_V7M_V6M */
+// Normal arm architecture not cortex
 
 
 /** Global IRQ enable */
@@ -76,6 +125,9 @@ unsigned isix_irq_save(void)
 		: "memory", "cc");
 	return flags;
 }
+
+
+#endif /* THUMB_V7M_V6M */
 
 
 /** Restore global interrupt status
