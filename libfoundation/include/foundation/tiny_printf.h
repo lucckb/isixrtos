5f506c56d92a691966f5fbee8be3691c6fd1a9c1
Lucjan Bryndza <lbryndza.p@boff.pl>
2017-06-20 21:48:12 +0200

    Tiny printf put char

--- libfoundation/include/foundation/tiny_printf.h
+++ libfoundation/include/foundation/tiny_printf.h
@@ -1,30 +1,30 @@
-/* ------------------------------------------------------------ */
+
 /*
  * printf.h
  *
  *  Created on: 2009-08-08
  *      Author: lucck
  */
-/* ------------------------------------------------------------ */
+
 #ifndef TINY_PRINTF_H_
 #define TINY_PRINTF_H_
-/* ------------------------------------------------------------ */
+
 #include <stddef.h>
-/* ------------------------------------------------------------ */
+
 #ifdef __cplusplus
 namespace fnd {
 extern "C" {
 #endif
 
-/* ------------------------------------------------------------ */
+
 /** Printf replacement without floating point */
 int tiny_printf(const char *format, ...) __attribute__ ((format (printf, 1, 2)));
 
-/* ------------------------------------------------------------ */
+
 
 /** Snprintf replacement without floating point aritmethic */
 int tiny_snprintf(char *out, unsigned long len,  const char *format, ...) __attribute__ ((format (printf, 3, 4)));
-/* ------------------------------------------------------------ */
+
 
 void register_printf_putc_handler_syslock(int (*fputc)(int, void*),void *arg,
 		void (*lock)(void),void (*unlock)(void));
@@ -34,7 +34,8 @@ static inline void register_printf_putc_handler(int (*fputc)(int, void*),void *a
 	register_printf_putc_handler_syslock(fputc, arg, NULL, NULL );
 }
 
-/* ------------------------------------------------------------ */
+int tiny_putchar( int ch );
+
 #ifdef __cplusplus
  }
  }
