c82daf8b2822725101c030023d55f0da36bf0e33
Lucjan Bryndza <lbryndza.p@boff.pl>
2019-08-10 13:19:31 +0200

    Log only basename without full path

--- libfoundation/include/foundation/sys/dbglog.h
+++ libfoundation/include/foundation/sys/dbglog.h
@@ -30,15 +30,25 @@
 
 #ifdef PDEBUG /* Debug enabled */
 #include <foundation/sys/tiny_printf.h>
+
+static inline const char* _isix_dbglog_extract_basename(const char *file) {
+	size_t pos;
+	for(pos=0; file&&file[pos]; ++pos) {}
+	while(pos>0) {
+		if((file[pos] == '/' || file[pos] == '\\')) return file+pos+1;
+		else --pos;
+	}
+	return file;
+}
+
 #pragma GCC system_header
 #ifdef __cplusplus
-
 #define dblog_init(function,arg, usart_init,...)	\
   do { fnd::register_printf_putc_handler(function,arg); \
   	   usart_init(__VA_ARGS__); } while(0)
 #define dblog_init_putc(function,arg) fnd::register_printf_putc_handler(function,arg)
 #define dblog_init_putc_locked(function,arg,lock,unlock) fnd::register_printf_putc_handler_syslock(function,arg,lock,unlock)
-#define dbprintf(fmt, ...) fnd::tiny_printf("%s:%d|" fmt "\r\n",__FILE__,__LINE__,## __VA_ARGS__)
+#define dbprintf(fmt, ...) fnd::tiny_printf("%s:%d|" fmt "\r\n",_isix_dbglog_extract_basename(__FILE__),__LINE__,## __VA_ARGS__)
 #define dblog_init_simple(function, arg ) fnd::register_printf_putc_handler(function,arg)
 #else /*__cplusplus */
 
@@ -50,7 +60,7 @@
 #define dblog_init_simple(function, arg ) register_printf_putc_handler(function,arg)
 
 //! Generic logger appi only if pdebug is defined
-#define dbprintf(fmt, ...) tiny_printf("%s:%d|" fmt "\r\n",__FILE__,__LINE__,## __VA_ARGS__)
+#define dbprintf(fmt, ...) tiny_printf("%s:%d|" fmt "\r\n",_isix_dbglog_extract_basename(__FILE__),__LINE__,## __VA_ARGS__)
 
 
 
