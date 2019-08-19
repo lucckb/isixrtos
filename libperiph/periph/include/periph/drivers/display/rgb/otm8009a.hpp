4b5e089d525a35c7869185ad6e6cd5cd75f26c6b
Lucjan Bryndza <lbryndza.p@boff.pl>
2019-08-19 18:42:34 +0200

    OTM driver initial version

--- libperiph/periph/include/periph/drivers/display/rgb/otm8009a.hpp
+++ libperiph/periph/include/periph/drivers/display/rgb/otm8009a.hpp
@@ -31,18 +31,39 @@ namespace periph::display {
             portrait,
             landscape
         };
+        enum class format: bool {
+            rgb888,
+            rgb565
+        };
         //! Constructors and destructors
         explicit otm8009a(bus::dsi& dsi, int dsi_chn);
         ~otm8009a() {}
         otm8009a(otm8009a&) = delete;
         otm8009a& operator=(otm8009a&) = delete;
         //! Open device
-        int open(orientation org) noexcept;
+        int open(orientation org, format fmt) noexcept;
         //! Close device
         int close() noexcept;
     private:
         //! Write command to the DSI interface
         int write_cmd(const uint8_t* args, size_t len) noexcept;
+        //! Write sequence
+        template <typename ...T> int write_seq(T&&... args) noexcept
+        {
+            uint8_t aargs[sizeof...(args)] = { uint8_t(args)... };
+            return write_cmd(aargs,sizeof aargs);
+        }
+        //! Write command
+        template <typename ...T> int write_cmd(uint16_t cmd, T&&... args) noexcept
+        {
+            int ret {};
+            static constexpr auto MCS_ADRSFT_ = 0x0000; /* W Address Shift Function (1) */
+            do {
+                ret = write_seq(MCS_ADRSFT_, cmd&0xff);
+                if(ret) break;
+                ret = write_seq(cmd>>8, args...);
+            } while(0);
+        }
     private:
         //! Private DSI bus for display
         bus::dsi& m_dsi;
--- libperiph/periph/src/display/rgb/otm8009a.cpp
+++ libperiph/periph/src/display/rgb/otm8009a.cpp
@@ -11,12 +11,14 @@
 
 #include <periph/drivers/display/rgb/otm8009a.hpp>
 #include <periph/drivers/display/bus/dsi.hpp>
+#include <isix/ostime.h>
+#include "otm8009a_regs.hpp"
 
 namespace periph::display {
 
 //! Constructor
 otm8009a::otm8009a(bus::dsi& dsi, int dsi_chn=0)
-    : m_dsi(dsi)
+    : m_dsi(dsi), m_dsichn(dsi_chn)
 {
 }
 
@@ -28,6 +30,182 @@ int otm8009a::write_cmd(const uint8_t* args, size_t len) noexcept
 		return m_dsi(m_dsichn, dsi::DCS_SHORT_PKT_WRITE_P1,args[0], args[1]); 
 	else
 		return m_dsi(m_dsichn, dsi::DCS_LONG_PKT_WRITE, args, len); 
+}
+
+//! Open device
+int otm8009a::open(orientation org, format fmt) noexcept
+{
+	int ret {};
+	do {
+		/* Enter CMD2 */
+		ret = write_cmd( MCS_CMD2_ENA1, 0x80, 0x09, 0x01);
+		if(ret) break;
+
+		/* Enter ORISE CMD2 */
+		ret = write_cmd( MCS_CMD2_ENA2, 0x80, 0x09);
+		if(ret) break;
+
+		ret = write_cmd( MCS_SD_PCH_CTRL, 0x30);
+		if(ret) break;
+		isix::wait_ms(10);
+
+		ret = write_cmd( MCS_NO_DOC1, 0x40);
+		if(ret) break;
+		isix::wait_ms(10);
+
+		ret = write_cmd( MCS_PWR_CTRL4 + 1, 0xA9);
+		if(ret) break;
+		ret = write_cmd( MCS_PWR_CTRL2 + 1, 0x34);
+		if(ret) break;
+		ret = write_cmd( MCS_P_DRV_M, 0x50);
+		if(ret) break;
+		ret = write_cmd( MCS_VCOMDC, 0x4E);
+		if(ret) break;
+		ret = write_cmd( MCS_OSC_ADJ, 0x66); /* 65Hz */
+		if(ret) break;
+		ret = write_cmd( MCS_PWR_CTRL2 + 2, 0x01);
+		if(ret) break;
+		ret = write_cmd( MCS_PWR_CTRL2 + 5, 0x34);
+		if(ret) break;
+		ret = write_cmd( MCS_PWR_CTRL2 + 4, 0x33);
+		if(ret) break;
+		ret = write_cmd( MCS_GVDDSET, 0x79, 0x79);
+		if(ret) break;
+		ret = write_cmd( MCS_SD_CTRL + 1, 0x1B);
+		if(ret) break;
+		ret = write_cmd( MCS_PWR_CTRL1 + 2, 0x83);
+		if(ret) break;
+		ret = write_cmd( MCS_SD_PCH_CTRL + 1, 0x83);
+		if(ret) break;
+		ret = write_cmd( MCS_RGB_VIDEO_SET, 0x0E);
+		if(ret) break;
+		ret = write_cmd( MCS_PANSET, 0x00, 0x01);
+		if(ret) break;
+
+		ret = write_cmd( MCS_GOAVST, 0x85, 0x01, 0x00, 0x84, 0x01, 0x00);
+		if(ret) break;
+		ret = write_cmd( MCS_GOACLKA1, 0x18, 0x04, 0x03, 0x39, 0x00, 0x00,
+			0x00, 0x18, 0x03, 0x03, 0x3A, 0x00, 0x00, 0x00);
+		if(ret) break;
+		ret = write_cmd( MCS_GOACLKA3, 0x18, 0x02, 0x03, 0x3B, 0x00, 0x00,
+			0x00, 0x18, 0x01, 0x03, 0x3C, 0x00, 0x00, 0x00);
+		if(ret) break;
+		ret = write_cmd( MCS_GOAECLK, 0x01, 0x01, 0x20, 0x20, 0x00, 0x00,
+			0x01, 0x02, 0x00, 0x00);
+		if(ret) break;
+
+		ret = write_cmd( MCS_NO_DOC2, 0x00);
+		if(ret) break;
+
+		ret = write_cmd( MCS_PANCTRLSET1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+		if(ret) break;
+		ret = write_cmd( MCS_PANCTRLSET2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0);
+		if(ret) break;
+		ret = write_cmd( MCS_PANCTRLSET3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0);
+		if(ret) break;
+		ret = write_cmd( MCS_PANCTRLSET4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+		if(ret) break;
+		ret = write_cmd( MCS_PANCTRLSET5, 0, 4, 4, 4, 4, 4, 0, 0, 0, 0,
+			0, 0, 0, 0, 0);
+		if(ret) break;
+		ret = write_cmd( MCS_PANCTRLSET6, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4,
+			4, 0, 0, 0, 0);
+		if(ret) break;
+		ret = write_cmd( MCS_PANCTRLSET7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+		if(ret) break;
+		ret = write_cmd( MCS_PANCTRLSET8, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+			0xFF, 0xFF, 0xFF, 0xFF, 0xFF);
+		if(ret) break;
+
+		ret = write_cmd( MCS_PANU2D1, 0x00, 0x26, 0x09, 0x0B, 0x01, 0x25,
+			0x00, 0x00, 0x00, 0x00);
+		if(ret) break;
+
+		ret = write_cmd( MCS_PANU2D2, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x26, 0x0A, 0x0C, 0x02);
+		if(ret) break;
+		ret = write_cmd( MCS_PANU2D3, 0x25, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00);
+		if(ret) break;
+		ret = write_cmd( MCS_PAND2U1, 0x00, 0x25, 0x0C, 0x0A, 0x02, 0x26,
+			0x00, 0x00, 0x00, 0x00);
+		if(ret) break;
+		ret = write_cmd( MCS_PAND2U2, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x25, 0x0B, 0x09, 0x01);
+		if(ret) break;
+		ret = write_cmd( MCS_PAND2U3, 0x26, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00);
+		if(ret) break;
+
+		ret = write_cmd( MCS_PWR_CTRL1 + 1, 0x66);
+		if(ret) break;
+
+		ret = write_cmd( MCS_NO_DOC3, 0x06);
+		if(ret) break;
+
+		ret = write_cmd( MCS_GMCT22P, 0x00, 0x09, 0x0F, 0x0E, 0x07, 0x10,
+			0x0B, 0x0A, 0x04, 0x07, 0x0B, 0x08, 0x0F, 0x10, 0x0A, 0x01);
+		if(ret) break;
+		ret = write_cmd( MCS_GMCT22N, 0x00, 0x09, 0x0F, 0x0E, 0x07, 0x10,
+			0x0B, 0x0A, 0x04, 0x07, 0x0B, 0x08, 0x0F, 0x10, 0x0A, 0x01);
+		if(ret) break;
+
+		/* Exit CMD2 */
+		ret = write_cmd( MCS_CMD2_ENA1, 0xFF, 0xFF, 0xFF);
+		if(ret) break;
+
+		ret = write_seq(OTM_CMD_NOP, 0x00);
+		if(ret) break;
+
+		ret = write_seq(OTM_CMD_SLPOUT);
+		if(ret) break;
+	
+		/* Wait for sleep out */
+		isix::wait_ms(120);
+		switch (fmt) {
+		case format::rgb888:
+			ret = write_seq(OTM_CMD_COLMOD, (COLMOD_VIPF_24_1 | COLMOD_IFPF_24));
+			break;
+		case format::rgb565:
+			ret = write_seq(OTM_CMD_COLMOD, (COLMOD_VIPF_16 | COLMOD_IFPF_16));
+		default:
+			break;
+		}
+		if(ret) break;
+
+		/*
+		* Default orientation is portrait,
+		* so configure landspace if required
+		*/
+		if (org == orientation::landscape) {
+			ret = write_seq(OTM_CMD_MADCTL, (MADCTL_MV | MADCTL_MX));
+			if(ret) break;
+			ret = write_seq(OTM_CMD_CASET, 0x00, 0x00, 0x03, 0x1F);
+			if(ret) break;
+			ret = write_seq(OTM_CMD_PASET, 0x00, 0x00, 0x01, 0xDF);
+			if(ret) break;
+		}
+
+		/* Content Adaptive Backlight Control */
+		ret = write_seq(OTM_CMD_WRDISBV, 0x7F);
+		if(ret) break;
+		ret = write_seq(OTM_CMD_WRCTRLD, 0x2C);
+		if(ret) break;
+		ret = write_seq(OTM_CMD_WRCABC, 0x02);
+		if(ret) break;
+		ret = write_seq(OTM_CMD_WRCABCMB, 0xFF);
+		if(ret) break;
+
+		ret = write_seq(OTM_CMD_DISPON, 0x00);
+		if(ret) break;
+		ret = write_seq(OTM_CMD_NOP, 0x00);
+		if(ret) break;
+		ret = write_seq(OTM_CMD_RAMWR, 0x00);
+		if(ret) break;
+	} while(0);
+	return ret;
 }
 
 }
