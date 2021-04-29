8fb423fc845d36b9806b167d69fbd4aa3c2c063f
Lucjan Bryndza <lbryndza.p@boff.pl>
2021-04-29 21:32:14 +0200

    Mempoll allocator throws exception

--- libisix/include/isix/cpp/mempool.hpp
+++ libisix/include/isix/cpp/mempool.hpp
@@ -18,66 +18,76 @@
 
 #pragma once
 
-/*-----------------------------------------------------------------------*/
 #ifdef __cplusplus
 #include <cstddef>
-/*-----------------------------------------------------------------------*/
+#include <new>
+#include <cstdlib>
+
 namespace isix {
-/*-----------------------------------------------------------------------*/
-/** Mempool class for pool memory management
- *  WARNING! This class is indented only for
- *  usage with POD types, and it is helper class only.
- */
-template <typename T> class mempool
-{
-public:
-	/** Constructor
-	 * @param[in] elems Number of elements
+	/** Mempool class for pool memory management
+	 *  WARNING! This class is indented only for
+	 *  usage with POD types, and it is helper class only.
 	 */
-	explicit mempool( size_t elems )
-		: m_mp( isix_mempool_create( elems, sizeof(T) ) )
+	template <typename T> class mempool
 	{
-	}
-	//! Destructor
-	~mempool()
-	{
-		isix_mempool_destroy( m_mp );
-	}
-	/** Alloc mempool element
-	 * @return allocated element
-	 */
-	T* alloc()
-	{
-		void* mem = isix_mempool_alloc(m_mp);
-		T* obj = new(mem) T;
-		return obj;
-	}
+		private:
+			inline void throw_or_abort(const void* mem)
+			{
+				if(!mem) {
+#	ifdef __EXCEPTIONS
+					throw std::bad_alloc();
+#	else
+					std::abort();
+#	endif
+				}
+			}
+		public:
+			/** Constructor
+			 * @param[in] elems Number of elements
+			 */
+			explicit mempool( size_t elems )
+				: m_mp( isix_mempool_create( elems, sizeof(T) ) )
+			{
+			}
+			//! Destructor
+			~mempool()
+			{
+				isix_mempool_destroy( m_mp );
+			}
+			/** Alloc mempool element
+			 * @return allocated element
+			 */
+			T* alloc()
+			{
+				void* mem = isix_mempool_alloc(m_mp);
+				throw_or_abort(mem);
+				T* obj = new(mem) T;
+				return obj;
+			}
 #if __cplusplus > 199711L
-	template <typename... ARGS>
-		T* alloc(ARGS... args) noexcept
-		{
-			void* mem = isix_mempool_alloc(m_mp);
-			T* obj = new(mem) T(args...);
-			return obj;
-		}
+			template <typename... ARGS>
+				T* alloc(ARGS&&... args) noexcept
+				{
+					void* mem = isix_mempool_alloc(m_mp);
+					throw_or_abort(mem);
+					T* obj = new(mem) T(std::forward<ARGS>(args)...);
+					return obj;
+				}
 #endif
-	int free( T* p )
-	{
-		p->~T();
-		return isix_mempool_free( m_mp, p );
-	}
-	/** Check if the fifo object is in valid state
-	 * @return true if object is ok else return false
-	 */
-	bool is_valid() { return m_mp!=0; }
-private:
-	osmempool_t m_mp;
-private:	//Noncopyable
-	mempool(const mempool&);
-	mempool& operator=(const mempool&);
-};
-/*-----------------------------------------------------------------------*/
+			int free( T* p )
+			{
+				p->~T();
+				return isix_mempool_free( m_mp, p );
+			}
+			/** Check if the fifo object is in valid state
+			 * @return true if object is ok else return false
+			 */
+			bool is_valid() { return m_mp!=0; }
+		private:
+			osmempool_t m_mp;
+		private:	//Noncopyable
+			mempool(const mempool&);
+			mempool& operator=(const mempool&);
+	};
 }
-/*-----------------------------------------------------------------------*/
 #endif /* __cplusplus */
-/*-----------------------------------------------------------------------*/
