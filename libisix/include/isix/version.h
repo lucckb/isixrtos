5ec47555b8cadb23555d55567146f1cb85ddbf7c
Tomek Sowiński <tomeksowi@gmail.com>
2023-12-07 22:53:31 +0100

    Initial Meson build setup
    For now only narrow path of stm32f405rg_qemu and compilation only, no linking, no dependencies.

--- libisix/include/isix/version.h
+++ libisix/include/isix/version.h
@@ -4,11 +4,11 @@
 #ifdef __cplusplus
 namespace isix {
 	static inline const char* get_version(void) {
-		return "@ISIX_VERSION@";
+		return ISIX_VERSION;
 	}
 }
 #else
 	static inline const char* isix_get_version(void) {
-		return "@ISIX_VERSION@";
+		return ISIX_VERSION;
 	}
 #endif
