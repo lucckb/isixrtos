e3571ba9e73e63acef43def52e8bb1a0885d927f
Tomek Sowiński <tomeksowi@gmail.com>
2024-02-25 16:04:18 +0100

    Fix indents

--- tests/libisix/01_basic_primitives.cpp
+++ tests/libisix/01_basic_primitives.cpp
@@ -20,10 +20,10 @@
 #include <isix/arch/sem_atomic.h>
 #include <foundation/sys/dbglog.h>
 #include "timer_interrupt.hpp"
+#include <isix_config.h>
 
 static const lest::test module[] =
 {
-
 	CASE("01_base_00 TimeBase timer vs systick")
 	{
 		static constexpr auto tim_hz = 1000U;
@@ -53,59 +53,54 @@ static const lest::test module[] =
 	},
 	CASE( "01_base_02 Atomic semaphore primitives test" )
 	{
-	_isix_port_atomic_sem_t sem;
-	_isix_port_atomic_sem_init( &sem , 1, sys_atomic_unlimited_value );
-	//Basic aritmetic tests
-	EXPECT( sem.value == 1 );
-	EXPECT( _isix_port_atomic_sem_inc( &sem ) == 2 );
-	EXPECT( sem.value == 2 );
-	EXPECT( _isix_port_atomic_sem_inc( &sem ) == 3 );
-	EXPECT( sem.value == 3 );
-	EXPECT( _isix_port_atomic_sem_dec( &sem ) == 2 );
-	EXPECT( sem.value == 2 );
-	_isix_port_atomic_sem_dec( &sem );
-	EXPECT( sem.value == 1 );
-	EXPECT( _isix_port_atomic_sem_dec( &sem ) == 0 );
-	EXPECT( _isix_port_atomic_sem_dec( &sem ) == -1 );
-	EXPECT( sem.value == -1 );
-	EXPECT( _isix_port_atomic_sem_dec( &sem ) == -2 );
-	EXPECT( sem.value == -2 );
-	// Testing try wait
-	EXPECT( _isix_port_atomic_sem_trydec( &sem ) == -2 );
-	EXPECT( sem.value == -2 );
-	//UP to 0
-	EXPECT( _isix_port_atomic_sem_inc( &sem ) == -1 );
-	EXPECT( _isix_port_atomic_sem_inc( &sem ) == 0 );
-	//Testing on 0 level
-	EXPECT( _isix_port_atomic_sem_trydec( &sem ) == 0);
-	EXPECT( sem.value == 0 );
-	//Testing on 1 level should be changed to 0
-	EXPECT( _isix_port_atomic_sem_inc( &sem ) == 1 );
-	//Testing on 1 level should be 0 again
-	EXPECT( _isix_port_atomic_sem_trydec( &sem ) == 1 );
-	EXPECT( sem.value == 0 );
-	//Testing for upper limit value
-	_isix_port_atomic_sem_t lsem;
-	_isix_port_atomic_sem_init( &lsem, 0, 3 );
-	EXPECT( _isix_port_atomic_sem_inc( &lsem ) == 1 );
-	EXPECT( lsem.value == 1 );
-	EXPECT( _isix_port_atomic_sem_inc( &lsem ) == 2 );
-	EXPECT( lsem.value == 2 );
-	EXPECT( _isix_port_atomic_sem_inc( &lsem ) == 3 );
-	EXPECT( lsem.value == 3 );
-	EXPECT( _isix_port_atomic_sem_inc( &lsem ) == 3 );
-	EXPECT( lsem.value == 3 );
-	EXPECT( _isix_port_atomic_sem_inc( &lsem ) == 3 );
-	EXPECT( lsem.value == 3 );
-	EXPECT( _isix_port_atomic_sem_dec( &lsem ) == 2 );
-	EXPECT( lsem.value == 2 );
+		_isix_port_atomic_sem_t sem;
+		_isix_port_atomic_sem_init( &sem , 1, sys_atomic_unlimited_value );
+		//Basic aritmetic tests
+		EXPECT( sem.value == 1 );
+		EXPECT( _isix_port_atomic_sem_inc( &sem ) == 2 );
+		EXPECT( sem.value == 2 );
+		EXPECT( _isix_port_atomic_sem_inc( &sem ) == 3 );
+		EXPECT( sem.value == 3 );
+		EXPECT( _isix_port_atomic_sem_dec( &sem ) == 2 );
+		EXPECT( sem.value == 2 );
+		_isix_port_atomic_sem_dec( &sem );
+		EXPECT( sem.value == 1 );
+		EXPECT( _isix_port_atomic_sem_dec( &sem ) == 0 );
+		EXPECT( _isix_port_atomic_sem_dec( &sem ) == -1 );
+		EXPECT( sem.value == -1 );
+		EXPECT( _isix_port_atomic_sem_dec( &sem ) == -2 );
+		EXPECT( sem.value == -2 );
+		// Testing try wait
+		EXPECT( _isix_port_atomic_sem_trydec( &sem ) == -2 );
+		EXPECT( sem.value == -2 );
+		//UP to 0
+		EXPECT( _isix_port_atomic_sem_inc( &sem ) == -1 );
+		EXPECT( _isix_port_atomic_sem_inc( &sem ) == 0 );
+		//Testing on 0 level
+		EXPECT( _isix_port_atomic_sem_trydec( &sem ) == 0);
+		EXPECT( sem.value == 0 );
+		//Testing on 1 level should be changed to 0
+		EXPECT( _isix_port_atomic_sem_inc( &sem ) == 1 );
+		//Testing on 1 level should be 0 again
+		EXPECT( _isix_port_atomic_sem_trydec( &sem ) == 1 );
+		EXPECT( sem.value == 0 );
+		//Testing for upper limit value
+		_isix_port_atomic_sem_t lsem;
+		_isix_port_atomic_sem_init( &lsem, 0, 3 );
+		EXPECT( _isix_port_atomic_sem_inc( &lsem ) == 1 );
+		EXPECT( lsem.value == 1 );
+		EXPECT( _isix_port_atomic_sem_inc( &lsem ) == 2 );
+		EXPECT( lsem.value == 2 );
+		EXPECT( _isix_port_atomic_sem_inc( &lsem ) == 3 );
+		EXPECT( lsem.value == 3 );
+		EXPECT( _isix_port_atomic_sem_inc( &lsem ) == 3 );
+		EXPECT( lsem.value == 3 );
+		EXPECT( _isix_port_atomic_sem_inc( &lsem ) == 3 );
+		EXPECT( lsem.value == 3 );
+		EXPECT( _isix_port_atomic_sem_dec( &lsem ) == 2 );
+		EXPECT( lsem.value == 2 );
 	}
 };
 
-
 extern lest::tests & specification();
 MODULE( specification(), module )
-
-
-
-
