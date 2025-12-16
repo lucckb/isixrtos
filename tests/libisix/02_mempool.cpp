ecc24f1aec4f7edfc65569313f3230ad758d3efd
Tomek Sowiński <tomeksowi@gmail.com>
2025-12-16 22:20:18 +0100

    mempool

--- tests/libisix/02_mempool.cpp
+++ tests/libisix/02_mempool.cpp
@@ -1,22 +1,6 @@
-/*
- * =====================================================================================
- *
- *       Filename:  mempool_test.cpp
- *
- *    Description:  Mempol tests
- *
- *        Version:  1.0
- *        Created:  24.06.2017 16:17:37
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Lucjan Bryndza (LB), lbryndza.p@boff.pl
- *   Organization:  BoFF
- *
- * =====================================================================================
- */
-
-#include <lest/lest.hpp>
+#define UNITY_FIXTURE_NO_EXTRAS
+#include <unity.h>
+#include <unity_fixture.h>
 #include <isix.h>
 #include <cstring>
 #include <foundation/sys/dbglog.h>
@@ -83,63 +67,72 @@ namespace
 }	//Unnamed namespace end
 
 
+TEST_GROUP(mempool);
+TEST_SETUP(mempool) {}
+TEST_TEAR_DOWN(mempool) {}
 
-
-const lest::test module[] =
+TEST(mempool, basic)
 {
-	CASE( "02_mempool_01 Mempool basic check" )
-	{
-		static constexpr size_t N_POOL = 14;
-		isix::mempool<pool> memp( N_POOL );
-		pool* pptr[N_POOL];
-		for(size_t n=0; n<N_POOL; ++n) {
-			auto p = memp.alloc(n+'A');
-			EXPECT( p != nullptr );
-			EXPECT( reinterpret_cast<long>(p)%ISIX_BYTE_ALIGNMENT_SIZE == 0U );
-			EXPECT( static_cast<bool>(*p) );
-			pptr[n] = p;
-		}
-		//Should return unable alloc
-#if defined(__EXCEPTIONS)
-		EXPECT_THROWS_AS( memp.alloc('X'), std::bad_alloc);
-#endif
-		//Free memory blocks
-		for(auto p : pptr ) {
-			EXPECT( memp.free(p) == ISIX_EOK );
-		}
-		//Now allocation should be possible
-		{
-			auto p = memp.alloc('X');
-			EXPECT(p!=nullptr);
-			EXPECT( memp.free(p) == ISIX_EOK );
-		}
-	},
-	CASE( "02_mempool_02 Mempool race condition check" )
-	{
-		static constexpr size_t N_POOL = 14;
-		isix::mempool<pool> memp( N_POOL );
-		task_test t1( memp );
-		task_test t2( memp );
-		task_test t3( memp );
-		task_test t4( memp );
-		task_test t5( memp );
-		task_test t6( memp );
-		t1.start();
-		t2.start();
-		t3.start();
-		t4.start();
-		t5.start();
-		t6.start();
-		isix_wait_ms(500);
-		EXPECT( t1.get() != nullptr);
-		EXPECT( t2.get() != nullptr);
-		EXPECT( t3.get() != nullptr);
-		EXPECT( t4.get() != nullptr);
-		EXPECT( t5.get() != nullptr);
-		EXPECT( t6.get() != nullptr);
+	static constexpr size_t N_POOL = 14;
+	isix::mempool<pool> memp( N_POOL );
+	pool* pptr[N_POOL];
+	for(size_t n=0; n<N_POOL; ++n) {
+		auto p = memp.alloc(n+'A');
+		TEST_ASSERT_NOT_NULL(p);
+		TEST_ASSERT_EQUAL(0, reinterpret_cast<long>(p)%ISIX_BYTE_ALIGNMENT_SIZE);
+		TEST_ASSERT(static_cast<bool>(*p));
+		pptr[n] = p;
 	}
-};
+	//Should return unable alloc
+#if defined(__EXCEPTIONS)
+	try {
+		memp.alloc('X');
+		TEST_ASSERT(false);
+	} catch (std::bad_alloc) {
+		TEST_ASSERT(true);
+	} catch (...) {
+		TEST_ASSERT(false);
+	}
+#endif
+	//Free memory blocks
+	for (auto p : pptr) {
+		TEST_ASSERT_EQUAL(ISIX_EOK, memp.free(p));
+	}
+	//Now allocation should be possible
+	{
+		auto p = memp.alloc('X');
+		TEST_ASSERT_NOT_NULL(p);
+		TEST_ASSERT_EQUAL(ISIX_EOK, memp.free(p));
+	}
+}
 
+TEST(mempool, race_condition)
+{
+	static constexpr size_t N_POOL = 14;
+	isix::mempool<pool> memp(N_POOL);
+	task_test t1(memp);
+	task_test t2(memp);
+	task_test t3(memp);
+	task_test t4(memp);
+	task_test t5(memp);
+	task_test t6(memp);
+	t1.start();
+	t2.start();
+	t3.start();
+	t4.start();
+	t5.start();
+	t6.start();
+	isix_wait_ms(500);
+	TEST_ASSERT_NOT_NULL(t1.get());
+	TEST_ASSERT_NOT_NULL(t2.get());
+	TEST_ASSERT_NOT_NULL(t3.get());
+	TEST_ASSERT_NOT_NULL(t4.get());
+	TEST_ASSERT_NOT_NULL(t5.get());
+	TEST_ASSERT_NOT_NULL(t6.get());
+}
 
-extern lest::tests & specification();
-MODULE( specification(), module )
+TEST_GROUP_RUNNER(mempool)
+{
+	RUN_TEST_CASE(mempool, basic);
+	RUN_TEST_CASE(mempool, race_condition);
+}
